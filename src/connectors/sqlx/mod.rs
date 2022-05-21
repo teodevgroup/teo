0ddd011a9536265d0528222ad1c90554c39d4464
Victor Teo <victor.teo@fillmula.com>
2022-05-21 20:50:53 +0800

    sqlx connector

--- src/connectors/sqlx/mod.rs
+++ src/connectors/sqlx/mod.rs
@@ -5,7 +5,7 @@ use std::sync::atomic::AtomicBool;
 use serde_json::{Value as JsonValue};
 use async_trait::async_trait;
 use sqlx::{Pool, Database};
-use crate::core::connector::Connector;
+use crate::core::connector::{Connector, ConnectorBuilder};
 use crate::core::graph::{Graph};
 use crate::core::object::Object;
 use crate::core::builders::GraphBuilder;
@@ -15,16 +15,12 @@ use crate::error::ActionError;
 
 #[derive(Debug)]
 pub struct SqlxConnector<DB> where DB: Database {
-    pool: Pool<DB>,
-    connected: AtomicBool
+    pool: Pool<DB>
 }
 
 impl<DB> SqlxConnector<DB> where DB: Database {
-    pub fn new(pool: Pool<DB>) -> Arc<SqlxConnector<DB>> {
-        Arc::new(SqlxConnector {
-            pool,
-            connected: AtomicBool::new(false),
-        })
+    pub(crate) fn new(pool: Pool<DB>, models: &Vec<Model>) -> SqlxConnector<DB> {
+        SqlxConnector { pool }
     }
 }
 
@@ -55,16 +51,34 @@ impl<DB> Connector for SqlxConnector<DB> where DB: Database {
         todo!()
     }
 }
-//
-// pub trait SqlxConnectorHelpers {
-//     fn sqlx<DB>(&mut self, pool: Pool<DB>) where DB: Database;
-// }
-//
-// impl SqlxConnectorHelpers for GraphBuilder {
-//
-//     fn sqlx<DB>(&mut self, pool: Pool<DB>) where DB: Database {
-//         self.connector_builder = Some(SqlxConnector::new(pool))
-//     }
-// }
+
+#[derive(Debug)]
+pub(crate) struct SqlxConnectorBuilder<DB> where DB: Database {
+    pool: Pool<DB>
+}
+
+impl<DB> SqlxConnectorBuilder<DB> where DB: Database {
+    pub(crate) fn new(pool: Pool<DB>) -> SqlxConnectorBuilder<DB> {
+        SqlxConnectorBuilder { pool }
+    }
+}
+
+#[async_trait]
+impl<DB> ConnectorBuilder for SqlxConnectorBuilder<DB> where DB: Database {
+    async fn build_connector(&self, models: &Vec<Model>) -> Box<dyn Connector> {
+        Box::new(SqlxConnector::new(self.pool.clone(), models))
+    }
+}
+
+pub trait SqlxConnectorHelpers {
+    fn sqlx<DB>(&mut self, pool: Pool<DB>) where DB: Database;
+}
+
+impl SqlxConnectorHelpers for GraphBuilder {
+
+    fn sqlx<DB>(&mut self, pool: Pool<DB>) where DB: Database {
+        self.connector_builder = Some(Box::new(SqlxConnectorBuilder::new(pool)))
+    }
+}
 
 unsafe impl<DB> Sync for SqlxConnector<DB> where DB: Database {}
