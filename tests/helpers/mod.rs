44ea9d4098fba7918e89ac5374e9ff1c13c7593b
Victor Teo <victor.teo@fillmula.com>
2022-06-27 18:16:38 +0800

    setup test format

--- tests/helpers/mod.rs
+++ tests/helpers/mod.rs
@@ -1,5 +1,6 @@
 use actix_http::Request;
-use actix_web::test::TestRequest;
+use actix_web::dev::{Service, ServiceResponse};
+use actix_web::test::{call_service, TestRequest};
 use serde_json::{json, Value as JsonValue};
 use regex::Regex;
 
@@ -8,10 +9,15 @@ pub fn is_object_id(value: &str) -> bool {
     regex.is_match(value)
 }
 
-pub fn request(url: &str, action: &str, body: JsonValue) -> Request {
+pub async fn request<S, B, E>(app: &S, url: &str, action: &str, body: JsonValue) -> S::Response where
+    S: Service<Request, Response = ServiceResponse<B>, Error = E>,
+    E: std::fmt::Debug,
+{
     let mut new_body = json!({
         "action": action
     });
-    new_body.as_object().unwrap().extend(body);
-    TestRequest::post().uri(&format!("/{url}/action")).set_json(new_body).to_request()
+    //new_body.as_object().unwrap().extend(body);
+    let req = TestRequest::post().uri(&format!("/{url}/action")).set_json(new_body).to_request();
+    call_service(&app, req).await
 }
+
--- tests/integration/connectors/mongodb/relations/one_optional_local_to_many_foreign.rs
+++ tests/integration/connectors/mongodb/relations/one_optional_local_to_many_foreign.rs
@@ -9,36 +9,6 @@ use teo::server::server::Server;
 use serde_json::json;
 use crate::helpers::request;
 
-lazy_static! {
-    static ref TEO_SERVER: AsyncOnce<&'static mut Server> = AsyncOnce::new(async {
-        let graph = Box::leak(Box::new(Graph::new(|g| {
-            g.data_source().mongodb("mongodb://localhost:27017/teotest_1ol_mf");
-            g.reset_database();
-            g.model("OneOptionalLocal", |m| {
-                m.field("id", |f| {
-                    f.primary().required().readonly().object_id().column_name("_id").auto();
-                });
-                m.field("foreignId", |f| {
-                    f.optional().object_id();
-                });
-                m.relation("foreign", |r| {
-                    r.optional().object("ManyForeign").fields(vec!["foreignId"]).references(vec!["id"]);
-                });
-            });
-            g.model("ManyForeign", |m| {
-                m.field("id", |f| {
-                    f.primary().required().readonly().object_id().column_name("_id").auto();
-                });
-                m.relation("locals", |r| {
-                    r.vec("OneOptionalLocal").fields(vec!["id"]).references(vec!["foreignId"]);
-                });
-            });
-            g.host_url("https://www.example.com");
-        }).await));
-        Box::leak(Box::new(Server::new(graph)))
-    });
-}
-
 async fn app() -> App<impl ServiceFactory<
     ServiceRequest,
     Response = ServiceResponse<BoxBody>,
@@ -46,14 +16,39 @@ async fn app() -> App<impl ServiceFactory<
     InitError = (),
     Error = Error,
 >> {
-    TEO_SERVER.get().await.make_app()
+    let graph = Box::leak(Box::new(Graph::new(|g| {
+        g.data_source().mongodb("mongodb://127.0.0.1:27017/teotest_1ol_mf");
+        g.reset_database();
+        g.model("OneOptionalLocal", |m| {
+            m.field("id", |f| {
+                f.primary().required().readonly().object_id().column_name("_id").auto();
+            });
+            m.field("foreignId", |f| {
+                f.optional().object_id();
+            });
+            m.relation("foreign", |r| {
+                r.optional().object("ManyForeign").fields(vec!["foreignId"]).references(vec!["id"]);
+            });
+        });
+        g.model("ManyForeign", |m| {
+            m.field("id", |f| {
+                f.primary().required().readonly().object_id().column_name("_id").auto();
+            });
+            m.relation("locals", |r| {
+                r.vec("OneOptionalLocal").fields(vec!["id"]).references(vec!["foreignId"]);
+            });
+        });
+        g.host_url("https://www.example.com");
+    }).await));
+    let server = Box::leak(Box::new(Server::new(graph)));
+    server.make_app()
 }
 
 #[test]
 #[serial]
 async fn create_with_nested_create() {
     let app = test::init_service(app().await).await;
-    let req = request("one-optional-locals", "Create", json!({
+    let res = request(&app, "one-optional-locals", "Create", json!({
         "create": {
             "foreign": {
                 "create": {}
@@ -62,9 +57,18 @@ async fn create_with_nested_create() {
         "include": {
             "foreign": true
         }
+    })).await;
+    assert_json_response!(res, json!({
+        "data": {
+            "matches": {
+                "id": {"is": "objectId"},
+                "foreignId": {"and": [{"is": "objectId"}, {"is": "$foreignId"}]},
+                "foreign": {
+                    "id": {"and": [{"is": "objectId"}, {"is": "$foreignId"}]},
+                }
+            }
+        }
     }));
-    let resp: ServiceResponse = test::call_service(&app, req).await;
-
 }
 
 #[test]
