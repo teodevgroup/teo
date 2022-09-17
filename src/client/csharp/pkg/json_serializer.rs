26a375c268ff65253dbb6ac725fcbd742cea91a2
Victor Teo <victor.teo@fillmula.com>
2022-09-17 09:56:35 +0800

    generate enum json converters for csharp

--- src/client/csharp/pkg/json_serializer.rs
+++ src/client/csharp/pkg/json_serializer.rs
@@ -1,6 +1,85 @@
+use inflector::Inflector;
 use crate::core::graph::Graph;
+use crate::core::r#enum::Enum;
 
-pub(crate) async fn generate_json_serializer_cs(_graph: &Graph) -> String {
+fn enum_read_cases(e: &Enum) -> String {
+    let enum_name = &e.name;
+    let mut retval = "".to_owned();
+    let spaces_string = " ".repeat(16);
+    let spaces = &spaces_string;
+    for c in &e.choices {
+        let c_value = &c.name;
+        let pascal = c_value.to_pascal_case();
+        retval += format!(r#"{spaces}case "{c_value}": {{"#).as_str();
+        retval.push('\n');
+        retval += format!("{spaces}    return {enum_name}.{pascal};\n").as_str();
+        retval += format!("{spaces}}}\n").as_str();
+    }
+    retval
+}
+
+fn enum_write_cases(e: &Enum) -> String {
+    let enum_name = &e.name;
+    let mut retval = "".to_owned();
+    let spaces_string = " ".repeat(16);
+    let spaces = &spaces_string;
+    for c in &e.choices {
+        let c_value = &c.name;
+        let pascal = c_value.to_pascal_case();
+        retval += format!(r#"{spaces}case {enum_name}.{pascal}: {{"#).as_str();
+        retval.push('\n');
+        retval += format!(r#"{spaces}    writer.WriteStringValue("{c_value}");"#).as_str();
+        retval.push('\n');
+        retval += format!("{spaces}    return;\n").as_str();
+        retval += format!("{spaces}}}\n").as_str();
+    }
+    retval
+}
+
+fn serializer_for_enum(e: &Enum) -> String {
+    let enum_name = &e.name;
+    let read_cases = enum_read_cases(e);
+    let write_cases = enum_write_cases(e);
+    format!(r#"    public class {enum_name}JsonConverter : JsonConverter<{enum_name}> {{
+
+        public override {enum_name} Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {{
+            switch (reader.GetString()) {{
+{read_cases}
+            }}
+            throw new NotImplementedException();
+        }}
+
+        public override void Write(Utf8JsonWriter writer, {enum_name} value, JsonSerializerOptions options) {{
+            switch (value) {{
+{write_cases}
+            }}
+        }}
+    }}
+"#)
+}
+
+fn serializer_for_enums(g: &Graph) -> String {
+    let mut retval = "".to_owned();
+    for (_, e) in g.enums() {
+        retval += serializer_for_enum(e).as_str();
+        retval.push('\n');
+    }
+    retval
+}
+
+fn enum_converters(g: &Graph) -> String {
+    let mut retval = "".to_owned();
+    for (_, e) in g.enums() {
+        let enum_name = &e.name;
+        retval += format!("            options.Converters.Add(new {enum_name}JsonConverter());").as_str();
+        retval.push('\n');
+    }
+    retval
+}
+
+pub(crate) async fn generate_json_serializer_cs(graph: &Graph) -> String {
+    let enums = serializer_for_enums(graph);
+    let enum_converters = enum_converters(graph);
     format!(r#"using System;
 using System.Reflection;
 using System.Text.Json;
@@ -159,19 +238,19 @@ namespace Teo {{
         }}
     }}
 
-    static public class JSJsonSerializer {{
+{enums}    static public class JSJsonSerializer {{
         static private JsonSerializerOptions options() {{
             var options = new JsonSerializerOptions {{
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
             }};
-            options.Converters.Add(new OneOfJsonConverterFactory());
             options.Converters.Add(new DateOnlyConverter());
             options.Converters.Add(new DateTimeConverter());
             options.Converters.Add(new DateTimeOffsetConverter());
+            options.Converters.Add(new OneOfJsonConverterFactory());
             options.Converters.Add(new OptionalJsonConverterFactory());
             options.Converters.Add(new EnumerableJsonConverterFactory());
-            return options;
+{enum_converters}            return options;
         }}
         static public string Serialize<T>(T value) {{
             return JsonSerializer.Serialize(value, options());
