ed09875be69259ea62addd022f400eabb23d8aea
Victor Teo <victor.teo@fillmula.com>
2022-09-18 09:37:35 +0800

    prelude

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,11 @@ pub mod core;
 pub mod connectors;
 pub mod client;
 pub mod app;
+
+pub mod prelude {
+    pub use crate::app::app::App;
+    pub use crate::app::run;
+    pub use crate::core::graph::Graph;
+    pub extern crate tokio;
+    pub use tokio::main;
+}
