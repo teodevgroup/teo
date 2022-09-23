4e1251903747bb50ae1d41e8d560d92131877347
Victor Teo <victor.teo@fillmula.com>
2022-09-23 21:39:21 +0800

    setup pool and build now

--- src/connectors/sql/connector.rs
+++ src/connectors/sql/connector.rs
@@ -1,9 +1,70 @@
-pub(crate) struct SQLConnector {
+use std::sync::Arc;
+use async_trait::async_trait;
+use sqlx::Database;
+use sqlx::pool::Pool;
+use serde_json::{Value as JsonValue};
+use crate::connectors::sql::query_builder::SQLDialect;
+use crate::core::model::Model;
+use sqlx::postgres::PgPoolOptions;
+use crate::core::connector::Connector;
+use crate::core::error::ActionError;
+use crate::core::save_session::SaveSession;
+use crate::prelude::{Graph, Object};
 
+#[derive(Debug)]
+pub(crate) struct SQLConnector<T: Database> {
+    pool: Pool<T>,
 }
 
-impl SQLConnector {
-    pub(crate) fn new() -> Self {
+impl<T: Database> SQLConnector<T> {
+    pub(crate) async fn new(url: String, models: &Vec<Model>, reset_database: bool) -> Self {
+        let pool: Pool<T> = Pool::connect(&url).await.unwrap();
+        Self::setup_database(&pool).await;
+        Self {
+            pool,
+        }
+    }
 
+    async fn setup_database(pool: &Pool<T>) {
+
+    }
+}
+
+#[async_trait]
+impl<T: Database> Connector for SQLConnector<T> {
+    async fn save_object(&self, object: &Object) -> Result<(), ActionError> {
+        todo!()
+    }
+
+    async fn delete_object(&self, object: &Object) -> Result<(), ActionError> {
+        todo!()
+    }
+
+    async fn find_unique(&self, graph: &Graph, model: &Model, finder: &JsonValue, mutation_mode: bool) -> Result<Object, ActionError> {
+        todo!()
+    }
+
+    async fn find_first(&self, graph: &Graph, model: &Model, finder: &JsonValue, mutation_mode: bool) -> Result<Object, ActionError> {
+        todo!()
+    }
+
+    async fn find_many(&self, graph: &Graph, model: &Model, finder: &JsonValue, mutation_mode: bool) -> Result<Vec<Object>, ActionError> {
+        todo!()
+    }
+
+    async fn count(&self, graph: &Graph, model: &Model, finder: &JsonValue) -> Result<usize, ActionError> {
+        todo!()
+    }
+
+    async fn aggregate(&self, graph: &Graph, model: &Model, finder: &JsonValue) -> Result<JsonValue, ActionError> {
+        todo!()
+    }
+
+    async fn group_by(&self, graph: &Graph, model: &Model, finder: &JsonValue) -> Result<JsonValue, ActionError> {
+        todo!()
+    }
+
+    fn new_save_session(&self) -> Arc<dyn SaveSession> {
+        todo!()
     }
 }
--- src/connectors/sql/connector_builder.rs
+++ src/connectors/sql/connector_builder.rs
@@ -1,4 +1,5 @@
 use async_trait::async_trait;
+use sqlx::Database;
 use crate::connectors::sql::connector::SQLConnector;
 use crate::connectors::sql::query_builder::SQLDialect;
 use crate::connectors::sql::inferred_types::mssql::inferred_database_type_mssql;
@@ -11,32 +12,34 @@ use crate::core::field::r#type::FieldType;
 use crate::core::model::Model;
 
 #[derive(Debug)]
-pub(crate) struct SQLConnectorBuilder {
-    database: SQLDialect,
+pub(crate) struct SQLConnectorBuilder<T: Database + Send + Sync> {
+    database: T,
+    dialect: SQLDialect,
     url: String,
 }
 
-impl SQLConnectorBuilder {
-    pub(crate) fn new(database: SQLDialect, url: String) -> Self {
+impl<T: Database + Send + Sync> SQLConnectorBuilder<T> {
+    pub(crate) fn new(database: T, dialect: SQLDialect, url: String) -> Self {
         Self {
             database,
+            dialect,
             url,
         }
     }
 }
 
 #[async_trait]
-impl ConnectorBuilder for SQLConnectorBuilder {
+impl<T: Database + Send + Sync> ConnectorBuilder for SQLConnectorBuilder<T> {
     fn inferred_database_type(&self, field_type: &FieldType) -> DatabaseType {
-        match self.database {
+        match self.dialect {
+            SQLDialect::SQLite => inferred_database_type_sqlite(field_type),
             SQLDialect::MySQL => inferred_database_type_mysql(field_type),
             SQLDialect::PostgreSQL => inferred_database_type_postgresql(field_type),
             SQLDialect::MSSQL => inferred_database_type_mssql(field_type),
-            SQLDialect::SQLite => inferred_database_type_sqlite(field_type),
         }
     }
 
     async fn build_connector(&self, models: &Vec<Model>, reset_database: bool) -> Box<dyn Connector> {
-        Box::new(SQLConnector::new(self.database, self.url.clone(), models, reset_database).await)
+        Box::new(SQLConnector::<T>::new(self.url.clone(), models, reset_database).await)
     }
 }
