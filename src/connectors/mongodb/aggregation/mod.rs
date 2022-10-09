1d90f46c403b3731e6b2f9cde6e949f2e52c6aa7
Victor Teo <victor.teo@fillmula.com>
2022-10-09 19:08:01 +0800

    rewrite mongodb aggregation

--- src/connectors/mongodb/aggregation/mod.rs
+++ src/connectors/mongodb/aggregation/mod.rs
@@ -13,6 +13,146 @@ use crate::tson;
 pub(crate) struct Aggregation { }
 
 impl Aggregation {
+
+    fn insert_group_set_unset_for_aggregate(model: &Model, group: &mut Document, set: &mut Document, unset: &mut Vec<String>, k: &str, g: &str, having_mode: bool) {
+        let prefix = if having_mode { "_having" } else { "" };
+        let dbk = if k == "_all" { "_all" } else {model.field(k).unwrap().column_name() };
+        if g == "count" {
+            if k == "_all" {
+                group.insert(format!("{prefix}_count__all"), doc!{"$count": {}});
+            } else {
+                group.insert(format!("{prefix}_count_{dbk}"), doc!{
+                "$sum": {
+                    "$cond": [{"$ifNull": [format!("${dbk}"), false]}, 1, 0]
+                }
+            });
+            }
+        } else {
+            group.insert(format!("{prefix}_{g}_{dbk}"), doc!{format!("${g}"): format!("${dbk}")});
+            if g == "sum" {
+                group.insert(format!("{prefix}_{g}_count_{dbk}"), doc!{format!("$sum"): {
+                "$cond": [
+                    {"$ifNull": [format!("${dbk}"), false]},
+                    1,
+                    0
+                ]
+            }});
+            }
+        }
+        if g == "sum" {
+            set.insert(format!("{prefix}_{g}.{k}"), doc!{
+            "$cond": {
+                "if": {
+                    "$eq": [format!("${prefix}_{g}_count_{dbk}"), 0]
+                },
+                "then": null,
+                "else": format!("${prefix}_{g}_{dbk}")
+            }
+        });
+            unset.push(format!("{prefix}_{g}_{dbk}"));
+            unset.push(format!("{prefix}_{g}_count_{dbk}"));
+        } else {
+            set.insert(format!("{prefix}_{g}.{k}"), format!("${prefix}_{g}_{dbk}"));
+            unset.push(format!("{prefix}_{g}_{dbk}"));
+        }
+    }
+
+    pub(crate) fn build_for_aggregate(model: &Model, graph: &Graph, value: &Value) -> ActionResult<Vec<Document>> {
+        let mut retval = Self::build(model, graph, value)?;
+        // group by contains it's own aggregates
+        let empty_aggregates = tson!({});
+        let the_aggregates = if by.is_some() {
+            if aggregates.is_none() {
+                Some(&empty_aggregates)
+            } else {
+                aggregates
+            }
+        } else {
+            aggregates
+        };
+        // $aggregates at last
+        if let Some(aggregates) = the_aggregates {
+            let mut group = if let Some(by) = by {
+                let mut id_for_group_by = doc!{};
+                for key in by.as_vec().unwrap() {
+                    let k = key.as_str().unwrap();
+                    let dbk = model.field(k).unwrap().column_name();
+                    id_for_group_by.insert(dbk, doc!{
+                    "$cond": [{"$ifNull": [format!("${dbk}"), false]}, format!("${dbk}"), null]
+                });
+                }
+                doc!{"_id": id_for_group_by}
+            } else {
+                doc!{"_id": Bson::Null}
+            };
+            let mut set = doc!{};
+            let mut unset: Vec<String> = vec![];
+            if let Some(by) = by {
+                for key in by.as_vec().unwrap() {
+                    let k = key.as_str().unwrap();
+                    let dbk = model.field(k).unwrap().column_name();
+                    set.insert(k, format!("$_id.{dbk}"));
+                }
+            }
+            if let Some(having) = having {
+                for (k, o) in having.as_hashmap().unwrap() {
+                    let _dbk = model.field(k).unwrap().column_name();
+                    for (g, _matcher) in o.as_hashmap().unwrap() {
+                        let g = g.strip_prefix("_").unwrap();
+                        Self::insert_group_set_unset_for_aggregate(model, &mut group, &mut set, &mut unset, k, g, true);
+                    }
+                }
+            }
+            for (g, o) in aggregates.as_hashmap().unwrap() {
+                let g = g.strip_prefix("_").unwrap();
+                for (k, _t) in o.as_hashmap().unwrap() {
+                    Self::insert_group_set_unset_for_aggregate(model, &mut group, &mut set, &mut unset, k, g, false);
+                }
+            }
+            retval.push(doc!{"$group": group});
+            retval.push(doc!{"$set": set});
+            if !unset.is_empty() {
+                retval.push(doc!{"$unset": unset});
+            }
+            // filter if there is a having
+            if let Some(having) = having {
+                let mut having_match = doc!{};
+                let mut having_unset: Vec<String> = Vec::new();
+                for (k, o) in having.as_hashmap().unwrap() {
+                    let dbk = model.field(k).unwrap().column_name();
+                    for (g, matcher) in o.as_hashmap().unwrap() {
+                        let g = g.strip_prefix("_").unwrap();
+                        let matcher_bson = parse_bson_where_entry(&FieldType::F64, matcher, graph, &(path + "having" + k + format!("_{g}")))?;
+                        having_match.insert(format!("_having_{g}.{dbk}"), matcher_bson);
+                        let having_group = format!("_having_{g}");
+                        if !having_unset.contains(&having_group) {
+                            having_unset.push(having_group);
+                        }
+                    }
+                }
+                retval.push(doc!{"$match": having_match});
+                retval.push(doc!{"$unset": having_unset});
+            }
+            let mut group_by_sort = doc!{};
+            if let Some(by) = by {
+                // we need to order these
+                for key in by.as_vec().unwrap() {
+                    let k = key.as_str().unwrap();
+                    group_by_sort.insert(k, 1);
+                }
+            }
+            if !group_by_sort.is_empty() {
+                retval.push(doc!{"$sort": group_by_sort});
+            }
+        }
+        Ok(retval)
+    }
+
+    pub(crate) fn build_for_count(model: &Model, graph: &Graph, value: &Value) -> ActionResult<Vec<Document>> {
+        let retval = Self::build(model, graph, value)?;
+        todo!("add code for count");
+        Ok(retval)
+    }
     pub(crate) fn build(model: &Model, graph: &Graph, value: &Value) -> ActionResult<Vec<Document>> {
         let mut retval: Vec<Document> = vec![];
         let r#where = value.get("where");
@@ -24,7 +164,6 @@ impl Aggregation {
         let page_number = value.get("pageNumber");
         let select = value.get("select");
         let include = value.get("include");
-
         // if cursor exists, we modify the actual where
         let cursor_where_additions = if let Some(cursor) = value.get("cursor") {
             let cursor = cursor.as_hashmap().unwrap();
@@ -142,7 +281,6 @@ impl Aggregation {
                 retval.append(&mut lookups);
             }
         }
-
         Ok(retval)
     }
 
