8f0dad604a1c90feafd511f324bdf9f3790c73d2
Victor Teo <victor.teo@fillmula.com>
2022-11-05 11:04:52 +0800

    update ts

--- src/generator/client/csharp/mod.rs
+++ src/generator/client/csharp/mod.rs
@@ -27,16 +27,16 @@ impl ClientGenerator for CSharpClientGenerator {
     }
 
     async fn generate_accessories(graph: &Graph, client: &Client, generator: &Generator) -> std::io::Result<()> {
-        ensure_directory("client").await?;
-        clear_directory("client/csharp").await?;
-        ensure_directory("client/csharp").await?;
-        generate_file("client/csharp/OneOf.cs", generate_one_of_cs(graph).await).await?;
-        generate_file("client/csharp/Optional.cs", generate_optional_cs(graph).await).await?;
-        generate_file("client/csharp/Enumerable.cs", generate_enumerable_cs(graph).await).await?;
-        generate_file("client/csharp/JsonSerializers.cs", generate_json_serializer_cs(graph).await).await?;
-        generate_file("client/csharp/Filters.cs", generate_filters_cs(graph).await).await?;
-        generate_file("client/csharp/Operations.cs", generate_operations_cs(graph).await).await?;
-        generate_file("client/csharp/SortOrder.cs", generate_sort_order_cs(graph).await).await?;
-        generate_file("client/csharp/Runtime.cs", generate_runtime_cs(graph, conf).await).await
+        generator.ensure_directory("client").await?;
+        generator.clear_directory("client/csharp").await?;
+        generator.ensure_directory("client/csharp").await?;
+        generator.generate_file("client/csharp/OneOf.cs", generate_one_of_cs(graph).await).await?;
+        generator.generate_file("client/csharp/Optional.cs", generate_optional_cs(graph).await).await?;
+        generator.generate_file("client/csharp/Enumerable.cs", generate_enumerable_cs(graph).await).await?;
+        generator.generate_file("client/csharp/JsonSerializers.cs", generate_json_serializer_cs(graph).await).await?;
+        generator.generate_file("client/csharp/Filters.cs", generate_filters_cs(graph).await).await?;
+        generator.generate_file("client/csharp/Operations.cs", generate_operations_cs(graph).await).await?;
+        generator.generate_file("client/csharp/SortOrder.cs", generate_sort_order_cs(graph).await).await?;
+        generator.generate_file("client/csharp/Runtime.cs", generate_runtime_cs(graph, conf).await).await
     }
 }
--- src/generator/client/typescript/mod.rs
+++ src/generator/client/typescript/mod.rs
@@ -1,19 +1,31 @@
 pub mod pkg;
 pub mod r#type;
-use crate::app::app::ClientConfiguration;
-use crate::client::shared::{clear_directory, ensure_directory, generate_file};
-use crate::client::typescript::pkg::src::filter_ts::generate_filter_ts;
-use crate::client::typescript::pkg::src::index_ts::generate_index_ts;
-use crate::client::typescript::pkg::src::operation_ts::generate_operation_ts;
-use crate::client::typescript::pkg::src::runtime_ts::generate_runtime_ts;
-use crate::core::graph::Graph;
 
-pub async fn generate_typescript_client(graph: &Graph, conf: &ClientConfiguration) -> std::io::Result<()> {
-    ensure_directory("client").await?;
-    clear_directory("client/typescript").await?;
-    ensure_directory("client/typescript/src").await?;
-    generate_file("client/typescript/src/filter.ts", generate_filter_ts(graph).await).await?;
-    generate_file("client/typescript/src/operation.ts", generate_operation_ts(graph).await).await?;
-    generate_file("client/typescript/src/runtime.ts", generate_runtime_ts(graph, conf).await).await?;
-    generate_file("client/typescript/src/index.ts", generate_index_ts(graph, conf).await).await
+use crate::app::app::ClientConfiguration;
+use crate::generator::client::shared::{clear_directory, ensure_directory, generate_file};
+use crate::generator::client::typescript::pkg::src::filter_ts::generate_filter_ts;
+use crate::generator::client::typescript::pkg::src::index_ts::generate_index_ts;
+use crate::generator::client::typescript::pkg::src::operation_ts::generate_operation_ts;
+use crate::generator::client::typescript::pkg::src::runtime_ts::generate_runtime_ts;
+use crate::core::conf::client::Client;
+use crate::core::graph::Graph;
+use crate::generator::client::ClientGenerator;
+use crate::generator::lib::generator::Generator;
+
+pub(crate) struct TypeScriptClientGenerator { }
+
+#[async_trait]
+impl ClientGenerator for TypeScriptClientGenerator {
+    async fn generate_main(graph: &Graph, client: &Client, generator: &Generator) -> std::io::Result<()> {
+        generate_file("client/typescript/src/index.ts", generate_index_ts(graph, conf).await).await
+    }
+
+    async fn generate_accessories(graph: &Graph, client: &Client, generator: &Generator) -> std::io::Result<()> {
+        ensure_directory("client").await?;
+        clear_directory("client/typescript").await?;
+        ensure_directory("client/typescript/src").await?;
+        generate_file("client/typescript/src/filter.ts", generate_filter_ts(graph).await).await?;
+        generate_file("client/typescript/src/operation.ts", generate_operation_ts(graph).await).await?;
+        generate_file("client/typescript/src/runtime.ts", generate_runtime_ts(graph, conf).await).await?
+    }
 }
