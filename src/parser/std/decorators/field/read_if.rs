905ca5781907d0c126197ff9d03b6e9b5f595df5
Victor Teo <victor.teo@fillmula.com>
2022-11-14 19:48:27 +0800

    finish read if and write if

--- src/parser/std/decorators/field/read_if.rs
+++ src/parser/std/decorators/field/read_if.rs
@@ -1,9 +1,7 @@
 use crate::core::field::Field;
 use crate::core::field::read_rule::ReadRule;
-use crate::core::field::write_rule::WriteRule;
 use crate::parser::ast::argument::Argument;
 
 pub(crate) fn read_if_decorator(args: Vec<Argument>, field: &mut Field) {
-    let arg = args.get(0).unwrap();
-    field.write_rule = WriteRule::NoWrite;
+    field.read_rule = ReadRule::ReadIf(args.get(0).unwrap().resolved.unwrap().as_pipeline().unwrap().clone());
 }
--- src/parser/std/decorators/field/write_if.rs
+++ src/parser/std/decorators/field/write_if.rs
@@ -1,8 +1,7 @@
 use crate::core::field::Field;
-use crate::core::field::read_rule::ReadRule;
 use crate::core::field::write_rule::WriteRule;
 use crate::parser::ast::argument::Argument;
 
-pub(crate) fn write_if_decorator(_args: Vec<Argument>, field: &mut Field) {
-    field.write_rule = WriteRule::NoWrite;
+pub(crate) fn write_if_decorator(args: Vec<Argument>, field: &mut Field) {
+    field.write_rule = WriteRule::WriteIf(args.get(0).unwrap().resolved.unwrap().as_pipeline().unwrap().clone());
 }
