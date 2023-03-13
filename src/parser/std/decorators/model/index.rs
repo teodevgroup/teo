06a0532d83e0c2264aa91d6e735e706a31ab376c
Victor Teo <victor.teo@fillmula.com>
2023-03-13 17:55:46 +0800

    model index decorators

--- src/parser/std/decorators/model/index.rs
+++ src/parser/std/decorators/model/index.rs
@@ -1,63 +1,30 @@
+use crate::core::field::Sort;
 use crate::core::model::builder::ModelBuilder;
+use crate::core::model::index::{ModelIndex, ModelIndexItem, ModelIndexType};
 
 use crate::parser::ast::argument::Argument;
 use crate::prelude::Value;
 
 static VALID_NAMES: [&str; 1] = ["map"];
 
-static FIELD_INDEX_PRIMARY: u8 = 0;
-static FIELD_INDEX_INDEX: u8 = 1;
-static FIELD_INDEX_UNIQUE: u8 = 2;
+static MODEL_INDEX_PRIMARY: u8 = 0;
+static MODEL_INDEX_INDEX: u8 = 1;
+static MODEL_INDEX_UNIQUE: u8 = 2;
 
 pub(crate) fn id_decorator(args: Vec<Argument>, model: &mut ModelBuilder) {
-    let arg_value = args.get(0).unwrap().resolved.as_ref().unwrap().as_value().unwrap();
-    match arg_value {
-        Value::String(str_value) => {
-            model.primary(vec![str_value]);
-        }
-        Value::Vec(vec_value) => {
-            let str_values: Vec<&str> = vec_value.iter().map(|v| v.as_raw_enum_choice().unwrap()).collect();
-            model.primary(str_values);
-        }
-        _ => {
-            panic!("Only string or array of string can be passed as @id's argument.")
-        }
-    }
+    decorator(args, model, MODEL_INDEX_PRIMARY)
 }
 
 pub(crate) fn index_decorator(args: Vec<Argument>, model: &mut ModelBuilder) {
-    let arg_value = args.get(0).unwrap().resolved.as_ref().unwrap().as_value().unwrap();
-    match arg_value {
-        Value::RawEnumChoice(str_value, _) => {
-            model.index(vec![str_value]);
-        }
-        Value::Vec(vec_value) => {
-            let str_values: Vec<&str> = vec_value.iter().map(|v| v.as_raw_enum_choice().unwrap()).collect();
-            model.index(str_values);
-        }
-        _ => {
-            panic!("Only string or array of string can be passed as @index's argument.")
-        }
-    }
+    decorator(args, model, MODEL_INDEX_INDEX)
 }
 
 pub(crate) fn unique_decorator(args: Vec<Argument>, model: &mut ModelBuilder) {
-    let arg_value = args.get(0).unwrap().resolved.as_ref().unwrap().as_value().unwrap();
-    match arg_value {
-        Value::RawEnumChoice(str_value, _) => {
-            model.unique(vec![str_value]);
-        }
-        Value::Vec(vec_value) => {
-            let str_values: Vec<&str> = vec_value.iter().map(|v| v.as_raw_enum_choice().unwrap()).collect();
-            model.unique(str_values);
-        }
-        _ => {
-            panic!("Only string or array of string can be passed as @unique's argument.")
-        }
-    }
+    decorator(args, model, MODEL_INDEX_UNIQUE)
 }
 
 fn decorator(args: Vec<Argument>, model: &mut ModelBuilder, index_kind: u8) {
+    let mut items: Vec<ModelIndexItem> = vec![];
     let mut map: Option<String> = None;
     if args.is_empty() {
         panic!("Model index decorator takes at least one argument.")
@@ -66,6 +33,23 @@ fn decorator(args: Vec<Argument>, model: &mut ModelBuilder, index_kind: u8) {
     if arg0.name.is_some() && (arg0.name.as_ref().unwrap().name.as_str() != "fields") {
         panic!("Model index decorator's first argument should be fields or no name.")
     }
+    let arg0_value = arg0.resolved.as_ref().unwrap().as_value().unwrap();
+    match arg0_value {
+        Value::Vec(vec) => {
+            for value in vec {
+                match value {
+                    Value::RawEnumChoice(name, args) => {
+                        items.push(model_index_item(name, args));
+                    }
+                    _ => unreachable!(),
+                }
+            }
+        }
+        Value::RawEnumChoice(name, args) => {
+            items.push(model_index_item(name, args));
+        }
+        _ => unreachable!(),
+    }
     // map name
     if let Some(arg1) = args.get(1) {
         if arg1.name.is_none() || (arg1.name.as_ref().unwrap().name.as_str() != "map") {
@@ -73,4 +57,40 @@ fn decorator(args: Vec<Argument>, model: &mut ModelBuilder, index_kind: u8) {
         }
         map = Some(arg1.resolved.as_ref().unwrap().as_value().unwrap().as_str().unwrap().to_owned());
     }
+    match index_kind {
+        0 => model.indices.push(ModelIndex::new(ModelIndexType::Primary, map, items)),
+        1 => model.indices.push(ModelIndex::new(ModelIndexType::Index, map, items)),
+        2 => model.indices.push(ModelIndex::new(ModelIndexType::Unique, map, items)),
+        _ => unreachable!(),
+    }
+}
+
+fn model_index_item(name: &String, args: &Option<Vec<(Option<String>, Value)>>) -> ModelIndexItem {
+    let name: String = name.clone();
+    let mut sort = Sort::Asc;
+    let mut len: Option<usize> = None;
+    if let Some(args) = args {
+        for (index, (arg_name, arg_value)) in args.iter().enumerate() {
+            match index {
+                0 => {
+                    if arg_name.is_some() && (arg_name.as_ref().unwrap().as_str() != "fields") {
+                        panic!("Unknown argument {}", arg_name.as_ref().unwrap());
+                    }
+                    match arg_value.as_raw_enum_choice().unwrap() {
+                        "asc" => sort = Sort::Asc,
+                        "desc" => sort = Sort::Desc,
+                        _ => unreachable!(),
+                    }
+                }
+                1 => {
+                    if arg_name.is_none() || (arg_name.as_ref().unwrap().as_str() != "length") {
+                        panic!("Second argument should be length.");
+                    }
+                    len = Some(arg_value.as_usize().unwrap());
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+    ModelIndexItem::new(name, Sort::Asc, None)
 }
