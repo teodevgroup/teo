21bdc317297ee655fd048000798c2f19335f088f
Victor Teo <victor.teo@fillmula.com>
2023-03-15 11:00:23 +0800

    remove old style tests

--- tests/integration/connectors/mongodb/queries/mod.rs
+++
@@ -1,8 +0,0 @@
-mod order_by;
-mod select;
-mod distinct;
-mod skip;
-mod take;
-mod r#where;
-mod aggregate;
-mod group_by;
--- tests/integration/connectors/mongodb/tests.rs
+++
@@ -1,8 +0,0 @@
-mod migrations;
-mod relations;
-mod data_types;
-mod queries;
-// mod integration_mongodb;
-// mod relation_mongodb;
-#[path = "../../../helpers/mod.rs"]
-mod helpers;
