362f5c5422497f95501a628c4fc76387ab1fb0c7
Xuechao Luan <xuechao.luan@autodesk.com>
2023-03-16 13:34:55 +0800

    Add word case pipeline for the string field

--- src/parser/std/pipeline/string/transform.rs
+++ src/parser/std/pipeline/string/transform.rs
@@ -6,6 +6,7 @@ use crate::core::pipeline::items::string::transform::pad_start::PadStartItem;
 use crate::core::pipeline::items::string::transform::regex_replace::RegexReplaceItem;
 use crate::core::pipeline::items::string::transform::split::SplitItem;
 use crate::core::pipeline::items::string::transform::trim::TrimItem;
+use crate::core::pipeline::items::string::transform::word_case::WordCaseItem;
 use crate::parser::ast::argument::Argument;
 
 pub(crate) fn regex_replace(args: Vec<Argument>) -> Arc<dyn Item> {
@@ -39,4 +40,8 @@ pub(crate) fn ellipsis(args: Vec<Argument>) -> Arc<dyn Item> {
     let arg1 = args.get(0).unwrap().resolved.as_ref().unwrap().as_value().unwrap();
     let arg2 = args.get(1).unwrap().resolved.as_ref().unwrap().as_value().unwrap();
     Arc::new(EllipsisItem::new(arg1.clone(), arg2))
+}
+
+pub(crate) fn word_case(_args: Vec<Argument>) -> Arc<dyn Item> {
+    Arc::new(WordCaseItem::new())
 }
