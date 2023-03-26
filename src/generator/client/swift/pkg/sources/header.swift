48a080cde1560baebd06ea2f346284085aa20d20
Victor Teo <victor.teo@fillmula.com>
2023-03-26 13:15:17 +0800

    client(swift): object id filter

--- src/generator/client/swift/pkg/sources/header.swift
+++ src/generator/client/swift/pkg/sources/header.swift
@@ -47,3 +47,129 @@ private func request<I: Encodable, O: Decodable>(model: String, action: String, 
     guard response is HTTPURLResponse else { fatalError("response format is unexpected") }
     return try! JSONDecoder().decode(O.self, from: data)
 }
+
+struct AnyEncodable: Encodable {
+    let value: any Encodable
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try! container.encode(value)
+    }
+}
+
+public enum NullOr<T>: Encodable where T: Encodable {
+    case null
+    case nonnull(T)
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .null:
+            try! container.encodeNil()
+        case .nonnull(let t):
+            try! container.encode(t)
+        }
+    }
+}
+
+extension NullOr: ExpressibleByUnicodeScalarLiteral where T: ExpressibleByUnicodeScalarLiteral {
+    public typealias UnicodeScalarLiteralType = T.UnicodeScalarLiteralType
+    public init(unicodeScalarLiteral value: T.UnicodeScalarLiteralType) {
+        self = .nonnull(T(unicodeScalarLiteral: value))
+    }
+}
+
+extension NullOr: ExpressibleByExtendedGraphemeClusterLiteral where T: ExpressibleByExtendedGraphemeClusterLiteral {
+    public typealias ExtendedGraphemeClusterLiteralType = T.ExtendedGraphemeClusterLiteralType
+    public init(extendedGraphemeClusterLiteral value: T.ExtendedGraphemeClusterLiteralType) {
+        self = .nonnull(T(extendedGraphemeClusterLiteral: value))
+    }
+}
+
+extension NullOr: ExpressibleByStringLiteral where T: ExpressibleByStringLiteral {
+    public typealias StringLiteralType = T.StringLiteralType
+    public init(stringLiteral value: T.StringLiteralType) {
+        self = .nonnull(T(stringLiteral: value))
+    }
+}
+
+public class ObjectIdFilter: Encodable, ExpressibleByStringLiteral {
+    public typealias StringLiteralType = String
+    public let equals: String?
+    public let `in`: [String]?
+    public let notIn: [String]?
+    public let lt: String?
+    public let lte: String?
+    public let gt: String?
+    public let gte: String?
+    public let not: ObjectIdFilter?
+    public init(
+        equals: String? = nil,
+        `in`: [String]? = nil,
+        notIn: [String]? = nil,
+        lt: String? = nil,
+        lte: String? = nil,
+        gt: String? = nil,
+        gte: String? = nil,
+        not: ObjectIdFilter? = nil
+    ) {
+        self.equals = equals
+        self.in = `in`
+        self.notIn = notIn
+        self.lt = lt
+        self.lte = lte
+        self.gt = gt
+        self.gte = gte
+        self.not = not
+    }
+    public required init(stringLiteral value: String) {
+        self.equals = value
+        self.in = nil
+        self.notIn = nil
+        self.lt = nil
+        self.lte = nil
+        self.gt = nil
+        self.gte = nil
+        self.not = nil
+    }
+}
+
+public class ObjectIdNullableFilter: Encodable, ExpressibleByStringLiteral {
+    public typealias StringLiteralType = String
+    public var equals: NullOr<String>?
+    public let `in`: [String?]?
+    public let notIn: [String?]?
+    public let lt: String?
+    public let lte: String?
+    public let gt: String?
+    public let gte: String?
+    public let not: ObjectIdNullableFilter?
+    public init(
+        equals: NullOr<String>? = nil,
+        `in`: [String?]? = nil,
+        notIn: [String?]? = nil,
+        lt: String? = nil,
+        lte: String? = nil,
+        gt: String? = nil,
+        gte: String? = nil,
+        not: ObjectIdNullableFilter? = nil
+    ) {
+        self.equals = equals
+        self.in = `in`
+        self.notIn = notIn
+        self.lt = lt
+        self.lte = lte
+        self.gt = gt
+        self.gte = gte
+        self.not = not
+    }
+    public required init(stringLiteral value: String) {
+        self.equals = .nonnull(value)
+        self.in = nil
+        self.notIn = nil
+        self.lt = nil
+        self.lte = nil
+        self.gt = nil
+        self.gte = nil
+        self.not = nil
+    }
+    public static var null = ObjectIdNullableFilter(equals: .null)
+}
