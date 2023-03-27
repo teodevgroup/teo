df73422c5acf5fa37361798637d98a88724e675f
Victor Teo <victor.teo@fillmula.com>
2023-03-27 22:28:46 +0800

    client(swift): where inputs

--- src/generator/client/swift/types.rs
+++ src/generator/client/swift/types.rs
@@ -22,6 +22,46 @@ pub(crate) fn field_type_to_swift_output_type(field_type: &FieldType) -> Cow<str
     }
 }
 
+pub(crate) fn field_type_to_swift_filter_type(field_type: &FieldType, optional: bool) -> Cow<str> {
+    match field_type {
+        FieldType::ObjectId => if optional { Cow::Borrowed("ObjectIdNullableFilter") } else { Cow::Borrowed("ObjectIdFilter") },
+        FieldType::Bool => if optional { Cow::Borrowed("BoolNullableFilter") } else { Cow::Borrowed("BoolFilter") },
+        FieldType::I32 => if optional { Cow::Borrowed("Int32NullableFilter") } else { Cow::Borrowed("Int32Filter") },
+        FieldType::I64 => if optional { Cow::Borrowed("Int64NullableFilter") } else { Cow::Borrowed("Int64Filter") },
+        FieldType::F32 => if optional { Cow::Borrowed("FloatNullableFilter") } else { Cow::Borrowed("FloatFilter") },
+        FieldType::F64 => if optional { Cow::Borrowed("DoubleNullableFilter") } else { Cow::Borrowed("DoubleFilter") },
+        FieldType::Decimal => if optional { Cow::Borrowed("DecimalNullableFilter") } else { Cow::Borrowed("DecimalFilter") },
+        FieldType::String => if optional { Cow::Borrowed("StringNullableFilter") } else { Cow::Borrowed("StringFilter") },
+        FieldType::Date => if optional { Cow::Borrowed("DateNullableFilter") } else { Cow::Borrowed("DateFilter") },
+        FieldType::DateTime => if optional { Cow::Borrowed("DateTimeNullableFilter") } else { Cow::Borrowed("DateTimeFilter") },
+        FieldType::Enum(enum_def) => Cow::Owned("Enum".to_owned() + if optional { "Nullable" } else { "" } + "Filter<" + enum_def.name() + ">"),
+        FieldType::Vec(inner) => Cow::Owned("Array".to_owned() + if optional { "Nullable" } else { "" } + "Filter<" + field_type_to_swift_output_type(inner.field_type()).as_ref() + if inner.is_optional() { "?" } else { "" } + ">"),
+        FieldType::HashMap(_) => unreachable!(),
+        FieldType::BTreeMap(_) => unreachable!(),
+        FieldType::Object(_) => unreachable!(),
+    }
+}
+
+pub(crate) fn field_type_to_swift_create_type(field_type: &FieldType, optional: bool) -> Cow<str> {
+    match field_type {
+        FieldType::ObjectId => if optional { Cow::Borrowed("NullOr<String>") } else { Cow::Borrowed("String") },
+        FieldType::Bool => if optional { Cow::Borrowed("NullOr<Bool>") } else { Cow::Borrowed("Bool") },
+        FieldType::I32 => if optional { Cow::Borrowed("NullOr<Int32>") } else { Cow::Borrowed("Int32") },
+        FieldType::I64 => if optional { Cow::Borrowed("NullOr<Int64>") } else { Cow::Borrowed("Int64") },
+        FieldType::F32 => if optional { Cow::Borrowed("NullOr<Float>") } else { Cow::Borrowed("Float") },
+        FieldType::F64 => if optional { Cow::Borrowed("NullOr<Double>") } else { Cow::Borrowed("Double") },
+        FieldType::Decimal => if optional { Cow::Borrowed("NullOr<Decimal>") } else { Cow::Borrowed("Decimal") },
+        FieldType::String => if optional { Cow::Borrowed("NullOr<String>") } else { Cow::Borrowed("String") },
+        FieldType::Date => if optional { Cow::Borrowed("NullOr<String>") } else { Cow::Borrowed("String") },
+        FieldType::DateTime => if optional { Cow::Borrowed("NullOr<Date>") } else { Cow::Borrowed("Date") },
+        FieldType::Enum(enum_def) => if optional { Cow::Owned("NullOr<".to_owned() + enum_def.name() + ">") } else { Cow::Borrowed(enum_def.name()) },
+        FieldType::Vec(inner) => Cow::Owned((if optional { "NullOr<[" } else { "[" }).to_owned() + field_type_to_swift_output_type(inner.field_type()).as_ref() + if inner.is_optional() { "?" } else { "" } + if optional { "]>" } else { ">" }),
+        FieldType::HashMap(_) => unreachable!(),
+        FieldType::BTreeMap(_) => unreachable!(),
+        FieldType::Object(_) => unreachable!(),
+    }
+}
+
 pub(crate) fn field_type_to_swift_vec(field_type: &str) -> Cow<str> {
     Cow::Owned("[".to_owned() + field_type + "]")
 }
--- src/generator/lib/shared/model_input.rs
+++ src/generator/lib/shared/model_input.rs
@@ -1,4 +1,6 @@
 use std::borrow::Cow;
+use itertools::Itertools;
+use crate::core::field::r#type::{FieldType, FieldTypeOwner};
 use crate::prelude::Graph;
 
 pub(crate) struct ModelInclude<'a> {
@@ -7,18 +9,45 @@ pub(crate) struct ModelInclude<'a> {
     pub(crate) many: bool,
 }
 
+pub(crate) struct ModelWhereField<'a> {
+    pub(crate) name: Cow<'a, str>,
+    pub(crate) filter_type: Cow<'a, str>,
+}
+
+pub(crate) struct ModelWhereUniqueField<'a> {
+    pub(crate) name: Cow<'a, str>,
+    pub(crate) create_type: Cow<'a, str>,
+}
+
 pub(crate) struct ModelInput<'a> {
     pub(crate) name: Cow<'a, str>,
     pub(crate) select: Vec<Cow<'a, str>>,
     pub(crate) includes: Vec<ModelInclude<'a>>,
+    pub(crate) where_fields: Vec<ModelWhereField<'a>>,
+    pub(crate) where_unique_fields: Vec<ModelWhereUniqueField<'a>>,
 }
 
-pub(crate) fn model_inputs(graph: &Graph) -> Vec<ModelInput> {
+pub(crate) fn model_inputs<F, G>(graph: &Graph, field_type_to_filter_type: F, field_type_to_create_type: G) -> Vec<ModelInput> where F: Fn(&FieldType, bool) -> Cow<str>, G: Fn(&FieldType, bool) -> Cow<str> {
     graph.models().iter().map(|m| {
         ModelInput {
             name: Cow::Borrowed(m.name()),
             select: m.output_keys().iter().filter(|k| m.field(k).is_some()).map(|k| Cow::Borrowed(k.as_str())).collect(),
             includes: m.relations().iter().map(|r| ModelInclude { relation_name: Cow::Borrowed(r.name()), model_name: Cow::Borrowed(r.model()), many: r.is_vec() }).collect(),
+            where_fields: m.query_keys().iter().map(|k| if let Some(field) = m.field(k) {
+                ModelWhereField {
+                    name: Cow::Borrowed(field.name()),
+                    filter_type: field_type_to_filter_type(field.field_type(), field.is_optional()),
+                }
+            } else if let Some(relation) = m.relation(k) {
+                ModelWhereField {
+                    name: Cow::Borrowed(relation.name()),
+                    filter_type: if relation.is_vec() { Cow::Owned(relation.model().to_owned() + "ListRelationFilter") } else { Cow::Owned(relation.model().to_owned() + "RelationFilter") }
+                }
+            } else { unreachable!()}).collect(),
+            where_unique_fields: m.indices().iter().filter(|i| i.r#type().is_unique()).map(|i| i.keys().iter().map(|k| m.field(k).unwrap()).map(|f| ModelWhereUniqueField {
+                name: Cow::Borrowed(f.name()),
+                create_type: field_type_to_create_type(f.field_type(), f.is_optional()),
+            })).flatten().dedup_by(|f1, f2| f1.name == f2.name).collect()
         }
     }).collect()
 }
