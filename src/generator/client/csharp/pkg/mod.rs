2fcec91573db3ffff90c8bbed6e1a9d13978940c
Victor Teo <victor.teo@fillmula.com>
2023-03-29 20:22:13 +0800

    client(csharp): remove old code

--- src/generator/client/csharp/pkg/mod.rs
+++ src/generator/client/csharp/pkg/mod.rs
@@ -1,9 +1,2 @@
-pub(crate) mod one_of;
-pub(crate) mod optional;
-pub(crate) mod enumerable;
-pub(crate) mod json_serializer;
-pub(crate) mod filters;
-pub(crate) mod operations;
-pub(crate) mod sort_order;
 pub(crate) mod runtime;
 pub(crate) mod index;
--- src/generator/client/csharp/pkg/runtime.rs
+++ src/generator/client/csharp/pkg/runtime.rs
@@ -1,54 +1,9 @@
 use crate::core::app::conf::ClientGeneratorConf;
 use crate::core::graph::Graph;
 
-
 pub(crate) async fn generate_runtime_cs(_graph: &Graph, client: &ClientGeneratorConf) -> String {
     let url = &client.host;
-    format!(r#"using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Windows.Web.Http;
-
-namespace Teo {{
-    public struct Response<D> {{
-        public D Data {{ get; set; }}
-    }}
-
-    public struct Response<M, D> {{
-        public M Meta {{ get; set; }}
-        public D Data {{ get; set; }}
-    }}
-
-    public struct ResponseError {{
-        public string Type {{ get; set; }}
-        public string Message {{ get; set; }}
-        public Dictionary<string, string>? Errors {{ get; set; }}
-    }}
-
-    public struct PagingInfo {{
-        public uint Count {{ get; set; }}
-        public uint? NumberOfPages {{ get; set; }}
-    }}
-
-    public struct TokenInfo {{
-        public string Token {{ get; set; }}
-    }}
-
-    public class TeoException : Exception {{
-
-        public override string Message {{ get; }}
-
-        public string Type {{ get; }}
-
-        public Dictionary<string, string>? Errors {{ get; }}
-
-        public TeoException(ResponseError responseError) {{
-            Message = responseError.Message;
-            Type = responseError.Type;
-            Errors = responseError.Errors;
-        }}
-    }}
-
+    format!(r#"
     public class Delegate {{
 
         protected static readonly string HOST = "{url}";
