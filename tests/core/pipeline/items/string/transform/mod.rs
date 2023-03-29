5eecbb6d8a0b7706bef17e4d7127fae3f17e1190
l1xnan <l1xnan@qq.com>
2023-03-29 21:50:12 +0800

    test: add pipline string transform function tests

---
+++ tests/core/pipeline/items/string/transform/mod.rs
@@ -0,0 +1,115 @@
+use test_helpers::*;
+
+#[before_all]
+#[after_all]
+mod test {
+    use std::sync::Mutex;
+    use serde_json::{json, Value};
+    use crate::lib::{ExecutionHandle, req};
+    use crate::lib::matcher_functions::{date_time_value, decimal_value};
+    use crate::{assert_json, matcher};
+    use once_cell::sync::Lazy;
+
+    static HANDLE: Lazy<Mutex<ExecutionHandle>> = Lazy::new(|| {
+        Mutex::new(ExecutionHandle::new())
+    });
+    static PORT: i32 = 4015;
+
+    fn before_all() {
+        HANDLE.lock().unwrap().execute(file!(), "serve");
+    }
+
+    fn after_all() {
+        HANDLE.lock().unwrap().exit();
+    }
+
+    #[test]
+    fn to_word_case() {
+        let res = req(PORT, "create", "Support", json!({
+            "create": {
+                "toWordCase": "fooBar",
+            },
+        }));
+        assert_json!(res, matcher!({
+            "data": {
+                "id": ignore,
+                "toWordCase": "foo bar",
+            }
+        }))
+    }
+
+    #[test]
+    fn to_lower_case() {
+        let res = req(PORT, "create", "Support", json!({
+            "create": {
+                "toLowerCase": "Foo中文BaR",
+            },
+        }));
+        assert_json!(res, matcher!({
+            "data": {
+                "id": ignore,
+                "toLowerCase": "foo中文bar",
+            }
+        }))
+    }
+
+    #[test]
+    fn to_upper_case() {
+        let res = req(PORT, "create", "Support", json!({
+            "create": {
+                "toUpperCase": "foo中文bar",
+            },
+        }));
+        assert_json!(res, matcher!({
+            "data": {
+                "id": ignore,
+                "toUpperCase": "FOO中文BAR",
+            }
+        }))
+    }
+
+    #[test]
+    fn to_sentence_case() {
+        let res = req(PORT, "create", "Support", json!({
+            "create": {
+                "toSentenceCase": "fooBar",
+            },
+        }));
+        assert_json!(res, matcher!({
+            "data": {
+                "id": ignore,
+                "toSentenceCase": "Foo bar",
+            }
+        }))
+    }
+
+    #[test]
+    fn to_title_case() {
+        let res = req(PORT, "create", "Support", json!({
+            "create": {
+                "toTitleCase": "foo bar",
+            },
+        }));
+        assert_json!(res, matcher!({
+            "data": {
+                "id": ignore,
+                "toTitleCase": "Foo Bar",
+            }
+        }))
+    }
+
+    #[test]
+    fn trim() {
+        let res = req(PORT, "create", "Support", json!({
+            "create": {
+                "trim": " abc def\t",
+            },
+        }));
+        assert_json!(res, matcher!({
+            "data": {
+                "id": ignore,
+                "trim": "abc def",
+            }
+        }))
+    }
+}
