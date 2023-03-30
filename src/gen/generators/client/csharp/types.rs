53e9eadcd7139f84c669b2e467d99d3a2c8abd81
Victor Teo <victor.teo@fillmula.com>
2023-03-30 11:21:07 +0800

    client: type lookup with more fields

--- src/gen/generators/client/csharp/types.rs
+++ src/gen/generators/client/csharp/types.rs
@@ -147,6 +147,14 @@ impl TypeLookup for CSharpTypes {
         Cow::Owned("Enumerate<".to_owned() + generated_type.as_ref() + ">")
     }
 
+    fn generated_type_to_optional<'a>(&self, generated_type: Cow<'a, str>) -> Cow<'a, str> {
+        Cow::Owned(generated_type.as_ref().to_owned() + "?")
+    }
+
+    fn generated_type_to_or_null<'a>(&self, generated_type: Cow<'a, str>) -> Cow<'a, str> {
+        Cow::Owned("Optional<".to_owned() + generated_type.as_ref() + ">")
+    }
+
     fn action_result_type<'a>(&self, action: Action, model_name: &'a str) -> Cow<'a, str> {
         Cow::Owned(match action.to_u32() {
             FIND_UNIQUE_HANDLER => format!("Response<{model_name}>?"),
@@ -169,6 +177,10 @@ impl TypeLookup for CSharpTypes {
     }
 
     fn number_type(&self) -> &'static str {
-        "Int"
+        "int"
+    }
+
+    fn bool_type(&self) -> &'static str {
+        "bool"
     }
 }
