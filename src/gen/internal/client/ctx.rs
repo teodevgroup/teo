0c9355dd83eab853b9772d10e72dc9731bb5c965
Victor Teo <victor.teo@fillmula.com>
2023-03-30 15:43:20 +0800

    client: create input fix

--- src/gen/internal/client/ctx.rs
+++ src/gen/internal/client/ctx.rs
@@ -1,9 +1,16 @@
 use crate::gen::interface::client::conf::Conf;
 use crate::gen::internal::client::outline::outline::Outline;
+use crate::gen::internal::type_lookup::TypeLookup;
 use crate::prelude::Graph;
 
 pub(in crate::gen) struct Ctx<'a> {
     pub(in crate::gen) conf: &'a Conf,
     pub(in crate::gen) graph: &'a Graph,
-    pub(in crate::gen) outline: &'a Outline,
+    pub(in crate::gen) outline: Outline<'a>,
+}
+
+impl<'a> Ctx<'a> {
+    pub(in crate::gen) fn build<L>(graph: &Graph, conf: &Conf, lookup: L) -> Self where L: TypeLookup {
+        Self { conf, graph, outline: Outline::new(graph, lookup) }
+    }
 }
