e392529d0fc1641b3e062e39cdab0f1e50868208
Victor Teo <victor.teo@fillmula.com>
2023-03-31 13:03:26 +0800

    feat(client(dart)): result type and gitignore

--- src/gen/generators/client/dart/types.rs
+++ src/gen/generators/client/dart/types.rs
@@ -1,6 +1,6 @@
 use std::borrow::Cow;
 use crate::core::action::{Action};
-use crate::core::field::r#type::{FieldType};
+use crate::core::field::r#type::{FieldType, FieldTypeOwner};
 use crate::gen::internal::type_lookup::TypeLookup;
 
 pub(crate) struct DartTypes { }
@@ -22,8 +22,30 @@ impl TypeLookup for DartTypes {
         todo!()
     }
 
-    fn field_type_to_result_type<'a>(&self, _field_type: &'a FieldType, _optional: bool) -> Cow<'a, str> {
-        todo!()
+    fn field_type_to_result_type<'a>(&self, field_type: &'a FieldType, optional: bool) -> Cow<'a, str> {
+        let base: Cow<str> = match field_type {
+            #[cfg(feature = "data-source-mongodb")]
+            FieldType::ObjectId => Cow::Borrowed("String"),
+            FieldType::String => Cow::Borrowed("String"),
+            FieldType::Bool => Cow::Borrowed("bool"),
+            FieldType::I32 => Cow::Borrowed("int"),
+            FieldType::I64 => Cow::Borrowed("long"),
+            FieldType::F32 => Cow::Borrowed("float"),
+            FieldType::F64 => Cow::Borrowed("double"),
+            FieldType::Decimal => Cow::Borrowed("decimal"),
+            FieldType::Date => Cow::Borrowed("DateOnly"),
+            FieldType::DateTime => Cow::Borrowed("DateTime"),
+            FieldType::Enum(enum_def) => Cow::Owned(enum_def.name().to_string()),
+            FieldType::Vec(inner) => Cow::Owned(self.field_type_to_result_type(inner.field_type(), inner.is_optional()).as_ref().to_owned() + "[]"),
+            FieldType::HashMap(_) => panic!(),
+            FieldType::BTreeMap(_) => panic!(),
+            FieldType::Object(name) => Cow::Owned(name.to_string()),
+        };
+        if optional {
+            Cow::Owned(base.as_ref().to_owned() + "?")
+        } else {
+            base
+        }
     }
 
     fn generated_type_to_vec<'a>(&self, _generated_type: Cow<'a, str>) -> Cow<'a, str> {
