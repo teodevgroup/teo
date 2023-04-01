c7e2f399cb6566a9ee2ca0cf62606ac01f524ce0
Victor Teo <victor.teo@fillmula.com>
2023-04-01 21:34:09 +0800

    feat(client/dart): gen delegates and object

--- src/gen/internal/client/outline/delegate.rs
+++ src/gen/internal/client/outline/delegate.rs
@@ -1,12 +1,29 @@
 use std::borrow::Cow;
 
-pub(crate) struct DelegateAction<'a> {
-    pub(crate) name: Cow<'a, str>,
-    pub(crate) response: Cow<'a, str>,
-    pub(crate) docs: Option<Cow<'a, str>>,
+pub(in crate::gen) struct DelegateAction<'a> {
+    pub(in crate::gen) name: &'static str,
+    pub(in crate::gen) response: Cow<'a, str>,
+    pub(in crate::gen) docs: Option<Cow<'a, str>>,
 }
 
-pub(crate) struct Delegate<'a> {
-    pub(crate) model_name: Cow<'a, str>,
-    pub(crate) actions: Vec<DelegateAction<'a>>,
+pub(in crate::gen) struct Delegate<'a> {
+    pub(in crate::gen) model_name: Cow<'a, str>,
+    pub(in crate::gen) actions: Vec<DelegateAction<'a>>,
+}
+
+impl<'a> Delegate<'a> {
+    pub(in crate::gen) fn value_for_data_transformer_dart(&self, action_name: &str) -> Cow<'a, str> {
+        match action_name {
+            "findUnique" | "findFirst" | "create" | "update" | "upsert" | "delete" | "signIn" | "identity" => Cow::Owned(format!("(p0) => {}.fromJson(p0)", self.model_name.as_ref())),
+            "findMany" | "createMany" | "updateMany" | "deleteMany" => Cow::Owned(format!("(p0) => p0.map((e) => {}.fromJson(e)).toList()", self.model_name.as_ref())),
+            _ => Cow::Borrowed("(p0) => p0"),
+        }
+    }
+    pub(in crate::gen) fn value_for_meta_transformer_dart(&self, action_name: &str) -> &'static str {
+        match action_name {
+            "findMany" | "createMany" | "updateMany" | "deleteMany" => "(p0) => PagingInfo.fromJson(p0)",
+            "signIn" => "(p0) => TokenInfo.fromJson(p0)",
+            _ => "null",
+        }
+    }
 }
