29dccc32ca740e1cfcc2a45520ad1848778f217b
Victor Teo <victor.teo@fillmula.com>
2023-04-02 09:46:59 +0800

    feat(client/dart): error handling

--- src/parser/std/decorators/field/foreign_key.rs
+++ src/parser/std/decorators/field/foreign_key.rs
@@ -4,4 +4,5 @@ use crate::parser::ast::argument::Argument;
 
 pub(crate) fn foreign_key_decorator(_args: Vec<Argument>, field: &mut Field) {
     field.foreign_key = true;
+    field.input_omissible = true;
 }
