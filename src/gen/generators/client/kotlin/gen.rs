a9a14bec93e43c8f4f0ecb13d5d5b8cfe0f26c97
Victor Teo <victor.teo@fillmula.com>
2023-04-03 20:32:49 +0800

    fix(client/kotlin): fix classes

--- src/gen/generators/client/kotlin/gen.rs
+++ src/gen/generators/client/kotlin/gen.rs
@@ -1,10 +1,18 @@
+use askama::Template;
 use async_trait::async_trait;
 use crate::gen::interface::client::conf::Conf;
 use crate::gen::internal::client::ctx::Ctx;
 use crate::gen::internal::client::generator::Generator;
+use crate::gen::internal::client::outline::outline::Outline;
 use crate::gen::internal::file_util::FileUtil;
+use crate::gen::internal::filters;
 
-
+#[derive(Template)]
+#[template(path = "client/kotlin/teo.kt.jinja", escape = "none")]
+pub(self) struct KotlinMainTemplate<'a> {
+    pub(self) outline: &'a Outline<'a>,
+    pub(self) conf: &'a Conf,
+}
 
 pub(crate) struct KotlinClientGenerator { }
 
@@ -15,18 +23,24 @@ impl KotlinClientGenerator {
 #[async_trait]
 impl Generator for KotlinClientGenerator {
     fn module_directory_in_package(&self, _conf: &Conf) -> String {
-        todo!()
+        "src/main/kotlin".to_owned()
     }
 
-    async fn generate_module_files(&self, _ctx: &Ctx, _generator: &FileUtil) -> std::io::Result<()> {
-        todo!()
+    async fn generate_module_files(&self, _ctx: &Ctx, generator: &FileUtil) -> std::io::Result<()> {
+        generator.clear_root_directory().await?;
+        Ok(())
     }
 
-    async fn generate_package_files(&self, _ctx: &Ctx, _generator: &FileUtil) -> std::io::Result<()> {
-        todo!()
+    async fn generate_package_files(&self, _ctx: &Ctx, generator: &FileUtil) -> std::io::Result<()> {
+        generator.ensure_root_directory().await?;
+        Ok(())
     }
 
-    async fn generate_main(&self, _ctx: &Ctx, _generator: &FileUtil) -> std::io::Result<()> {
-        todo!()
+    async fn generate_main(&self, ctx: &Ctx, generator: &FileUtil) -> std::io::Result<()> {
+        generator.generate_file(format!("{}.kt", ctx.conf.inferred_package_name_snake_case()), KotlinMainTemplate {
+            outline: &ctx.outline,
+            conf: ctx.conf,
+        }.render().unwrap()).await?;
+        Ok(())
     }
 }
