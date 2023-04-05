a492f5fcbbf99b5fad9f17448cb9271f54577816
Victor Teo <victor.teo@fillmula.com>
2023-04-05 16:08:25 +0800

    feat(client/swift): fix gen run

--- src/gen/generators/client/swift/types.rs
+++ src/gen/generators/client/swift/types.rs
@@ -10,67 +10,60 @@ impl SwiftTypes {
 }
 
 impl TypeLookup for SwiftTypes {
-    fn field_type_to_filter_type<'a>(&self, field_type: &'a FieldType, optional: bool) -> Cow<'a, str> {
-        match field_type {
-            FieldType::ObjectId => if optional { Cow::Borrowed("ObjectIdNullableFilter") } else { Cow::Borrowed("ObjectIdFilter") },
-            FieldType::Bool => if optional { Cow::Borrowed("BoolNullableFilter") } else { Cow::Borrowed("BoolFilter") },
-            FieldType::I32 => if optional { Cow::Borrowed("Int32NullableFilter") } else { Cow::Borrowed("Int32Filter") },
-            FieldType::I64 => if optional { Cow::Borrowed("Int64NullableFilter") } else { Cow::Borrowed("Int64Filter") },
-            FieldType::F32 => if optional { Cow::Borrowed("FloatNullableFilter") } else { Cow::Borrowed("FloatFilter") },
-            FieldType::F64 => if optional { Cow::Borrowed("DoubleNullableFilter") } else { Cow::Borrowed("DoubleFilter") },
-            FieldType::Decimal => if optional { Cow::Borrowed("DecimalNullableFilter") } else { Cow::Borrowed("DecimalFilter") },
-            FieldType::String => if optional { Cow::Borrowed("StringNullableFilter") } else { Cow::Borrowed("StringFilter") },
-            FieldType::Date => if optional { Cow::Borrowed("DateNullableFilter") } else { Cow::Borrowed("DateFilter") },
-            FieldType::DateTime => if optional { Cow::Borrowed("DateTimeNullableFilter") } else { Cow::Borrowed("DateTimeFilter") },
-            FieldType::Enum(enum_def) => Cow::Owned("Enum".to_owned() + if optional { "Nullable" } else { "" } + "Filter<" + enum_def.name() + ">"),
-            FieldType::Vec(inner) => Cow::Owned("Array".to_owned() + if optional { "Nullable" } else { "" } + "Filter<" + self.field_type_to_result_type(inner.field_type()).as_ref() + if inner.is_optional() { "?" } else { "" } + ">"),
-            FieldType::HashMap(_) => unreachable!(),
-            FieldType::BTreeMap(_) => unreachable!(),
-            FieldType::Object(_) => unreachable!(),
+    fn field_type_to_filter_type<'a>(&self, _field_type: &'a FieldType, _nullable: bool) -> Cow<'a, str> {
+        Cow::Borrowed("AnyEncodable")
+    }
+
+    fn field_type_to_filter_with_aggregates_type<'a>(&self, _field_type: &'a FieldType, _nullable: bool) -> Cow<'a, str> {
+        Cow::Borrowed("AnyEncodable")
+    }
+
+    fn field_type_to_create_type<'a>(&self, field_type: &'a FieldType, nullable: bool) -> Cow<'a, str> {
+        if nullable {
+            Cow::Borrowed("AnyEncodable")
+        } else {
+            match field_type {
+                FieldType::ObjectId => Cow::Borrowed("String"),
+                FieldType::Bool => Cow::Borrowed("Bool"),
+                FieldType::I32 => Cow::Borrowed("Int32"),
+                FieldType::I64 => Cow::Borrowed("Int64"),
+                FieldType::F32 => Cow::Borrowed("Float"),
+                FieldType::F64 => Cow::Borrowed("Double"),
+                FieldType::Decimal => Cow::Borrowed("Decimal"),
+                FieldType::String => Cow::Borrowed("String"),
+                FieldType::Date => Cow::Borrowed("String"),
+                FieldType::DateTime => Cow::Borrowed("Date"),
+                FieldType::Enum(enum_def) => Cow::Borrowed(enum_def.name()),
+                FieldType::Vec(inner) => Cow::Owned(("[").to_owned() + self.field_type_to_result_type(inner.field_type()).as_ref() + ">"),
+                FieldType::HashMap(_) => unreachable!(),
+                FieldType::BTreeMap(_) => unreachable!(),
+                FieldType::Object(_) => unreachable!(),
+            }
         }
+
     }
 
-    fn field_type_to_filter_with_aggregates_type<'a>(&self, field_type: &'a FieldType, nullable: bool) -> Cow<'a, str> {
-        Cow::Borrowed("Any")
-    }
-
-    fn field_type_to_create_type<'a>(&self, field_type: &'a FieldType, optional: bool) -> Cow<'a, str> {
-        match field_type {
-            FieldType::ObjectId => if optional { Cow::Borrowed("NullOr<String>") } else { Cow::Borrowed("String") },
-            FieldType::Bool => if optional { Cow::Borrowed("NullOr<Bool>") } else { Cow::Borrowed("Bool") },
-            FieldType::I32 => if optional { Cow::Borrowed("NullOr<Int32>") } else { Cow::Borrowed("Int32") },
-            FieldType::I64 => if optional { Cow::Borrowed("NullOr<Int64>") } else { Cow::Borrowed("Int64") },
-            FieldType::F32 => if optional { Cow::Borrowed("NullOr<Float>") } else { Cow::Borrowed("Float") },
-            FieldType::F64 => if optional { Cow::Borrowed("NullOr<Double>") } else { Cow::Borrowed("Double") },
-            FieldType::Decimal => if optional { Cow::Borrowed("NullOr<Decimal>") } else { Cow::Borrowed("Decimal") },
-            FieldType::String => if optional { Cow::Borrowed("NullOr<String>") } else { Cow::Borrowed("String") },
-            FieldType::Date => if optional { Cow::Borrowed("NullOr<String>") } else { Cow::Borrowed("String") },
-            FieldType::DateTime => if optional { Cow::Borrowed("NullOr<Date>") } else { Cow::Borrowed("Date") },
-            FieldType::Enum(enum_def) => if optional { Cow::Owned("NullOr<".to_owned() + enum_def.name() + ">") } else { Cow::Borrowed(enum_def.name()) },
-            FieldType::Vec(inner) => Cow::Owned((if optional { "NullOr<[" } else { "[" }).to_owned() + self.field_type_to_result_type(inner.field_type()).as_ref() + if inner.is_optional() { "?" } else { "" } + if optional { "]>" } else { ">" }),
-            FieldType::HashMap(_) => unreachable!(),
-            FieldType::BTreeMap(_) => unreachable!(),
-            FieldType::Object(_) => unreachable!(),
-        }
-    }
-
-    fn field_type_to_update_type<'a>(&self, field_type: &'a FieldType, optional: bool) -> Cow<'a, str> {
-        match field_type {
-            FieldType::ObjectId => if optional { Cow::Borrowed("NullOr<String>") } else { Cow::Borrowed("String") },
-            FieldType::Bool => if optional { Cow::Borrowed("NullOr<Bool>") } else { Cow::Borrowed("Bool") },
-            FieldType::I32 => if optional { Cow::Borrowed("NullOr<Int32>") } else { Cow::Borrowed("Int32") },
-            FieldType::I64 => if optional { Cow::Borrowed("NullOr<Int64>") } else { Cow::Borrowed("Int64") },
-            FieldType::F32 => if optional { Cow::Borrowed("NullOr<Float>") } else { Cow::Borrowed("Float") },
-            FieldType::F64 => if optional { Cow::Borrowed("NullOr<Double>") } else { Cow::Borrowed("Double") },
-            FieldType::Decimal => if optional { Cow::Borrowed("NullOr<Decimal>") } else { Cow::Borrowed("Decimal") },
-            FieldType::String => if optional { Cow::Borrowed("NullOr<String>") } else { Cow::Borrowed("String") },
-            FieldType::Date => if optional { Cow::Borrowed("NullOr<String>") } else { Cow::Borrowed("String") },
-            FieldType::DateTime => if optional { Cow::Borrowed("NullOr<Date>") } else { Cow::Borrowed("Date") },
-            FieldType::Enum(enum_def) => if optional { Cow::Owned("NullOr<".to_owned() + enum_def.name() + ">") } else { Cow::Borrowed(enum_def.name()) },
-            FieldType::Vec(inner) => Cow::Owned((if optional { "NullOr<[" } else { "[" }).to_owned() + self.field_type_to_result_type(inner.field_type()).as_ref() + if inner.is_optional() { "?" } else { "" } + if optional { "]>" } else { ">" }),
-            FieldType::HashMap(_) => unreachable!(),
-            FieldType::BTreeMap(_) => unreachable!(),
-            FieldType::Object(_) => unreachable!(),
+    fn field_type_to_update_type<'a>(&self, field_type: &'a FieldType, nullable: bool) -> Cow<'a, str> {
+        if nullable {
+            Cow::Borrowed("AnyEncodable")
+        } else {
+            match field_type {
+                FieldType::ObjectId => Cow::Borrowed("String"),
+                FieldType::Bool => Cow::Borrowed("Bool"),
+                FieldType::I32 => Cow::Borrowed("AnyEncodable"),
+                FieldType::I64 => Cow::Borrowed("AnyEncodable"),
+                FieldType::F32 => Cow::Borrowed("AnyEncodable"),
+                FieldType::F64 => Cow::Borrowed("AnyEncodable"),
+                FieldType::Decimal => Cow::Borrowed("AnyEncodable"),
+                FieldType::String => Cow::Borrowed("String"),
+                FieldType::Date => Cow::Borrowed("String"),
+                FieldType::DateTime => Cow::Borrowed("Date"),
+                FieldType::Enum(enum_def) => Cow::Borrowed(enum_def.name()),
+                FieldType::Vec(_inner) => Cow::Borrowed("AnyEncodable"),
+                FieldType::HashMap(_) => unreachable!(),
+                FieldType::BTreeMap(_) => unreachable!(),
+                FieldType::Object(_) => unreachable!(),
+            }
         }
     }
 
