3b0612e14403ea6bb60885b616500d5e61447d06
Victor Teo <victor.teo@fillmula.com>
2023-04-09 17:51:19 +0800

    feat(test): resolve test and debug

--- src/parser/ast/reference.rs
+++ src/parser/ast/reference.rs
@@ -2,6 +2,7 @@
 pub(crate) enum Reference {
     ModelReference((usize, usize, String)),
     ConstantReference((usize, usize)),
+    DataSetReference((usize, usize)),
 }
 
 impl Reference {
@@ -9,7 +10,7 @@ impl Reference {
     pub(crate) fn as_model_ref(&self) -> Option<(usize, usize, String)> {
         match self {
             Reference::ModelReference(r) => Some(r.clone()),
-            Reference::ConstantReference(_) => None,
+            _ => None,
         }
     }
 
@@ -19,12 +20,23 @@ impl Reference {
 
     pub(crate) fn as_constant_ref(&self) -> Option<(usize, usize)> {
         match self {
-            Reference::ModelReference(_) => None,
             Reference::ConstantReference(c) => Some(c.clone()),
+            _ => None,
         }
     }
 
     pub(crate) fn is_constant_ref(&self) -> bool {
         self.as_constant_ref().is_some()
+    }
+
+    pub(crate) fn as_dataset_ref(&self) -> Option<(usize, usize)> {
+        match self {
+            Reference::DataSetReference(c) => Some(c.clone()),
+            _ => None,
+        }
+    }
+
+    pub(crate) fn is_dataset_ref(&self) -> bool {
+        self.as_dataset_ref().is_some()
     }
 }
