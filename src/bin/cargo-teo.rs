0b7e20cc3bb51ac048f492df98f540d538408259
Victor Teo <victor.teo@fillmula.com>
2023-10-20 05:46:15 +0800

    use teo result and fix compile

--- src/bin/cargo-teo.rs
+++ src/bin/cargo-teo.rs
@@ -1,12 +1,10 @@
 use tokio::main;
-use teo::app::entrance::Entrance;
-use teo::core::result::Result;
-use teo::app::app::App;
-use teo::app::app_ctx::AppCtx;
+use teo_result::Result;
 
 #[main]
 async fn main() -> Result<()> {
-    let app = App::new()?;
-    AppCtx::get()?.set_entrance(Entrance::CLI)?;
-    app.run().await
+    // let app = App::new()?;
+    // AppCtx::get()?.set_entrance(Entrance::CLI)?;
+    // app.run().await
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,39 @@
-pub mod core;
-pub(crate) mod parser;
-pub(crate) mod connectors;
-pub(crate) mod gen;
-pub(crate) mod seeder;
-pub(crate) mod purger;
-pub mod app;
-pub(crate) mod server;
-pub(crate) mod migrate;
-pub(crate) mod run;
-
-pub mod prelude {
-    pub use crate::app::app::App;
-    pub use crate::app::routes::middleware_ctx::*;
-    pub use crate::app::routes::action_ctx::*;
-    pub use crate::app::routes::*;
-    pub use crate::app::routes::req_local::*;
-    pub use crate::app::routes::res::*;
-    pub use crate::app::routes::req::*;
-    pub use crate::app::routes::readonly_header_map::*;
-    pub use crate::server::ReqCtx;
-    pub use crate::app::routes::req::Req;
-    pub use crate::app::routes::res::Res;
-    pub use crate::core::graph::Graph;
-    pub use teo_teon::value::Value;
-    pub use teo_teon::teon;
-    pub use teo_teon::teon_vec;
-    pub use teo_teon::teon_unexpected;
-    pub use teo_teon::teon_expect_expr_comma;
-    pub use crate::core::object::Object;
-    pub extern crate tokio;
-    pub use tokio::main;
-    pub extern crate key_path;
-    pub use key_path::path;
-    pub use crate::core::result::Result;
-    pub use crate::core::error::Error;
-    pub use crate::core::ctx::model::ModelCtx;
-    pub use crate::core::ctx::user::UserCtx;
-}
+// pub mod core;
+// pub(crate) mod parser;
+// pub(crate) mod connectors;
+// pub(crate) mod gen;
+// pub(crate) mod seeder;
+// pub(crate) mod purger;
+// pub mod app;
+// pub(crate) mod server;
+// pub(crate) mod migrate;
+// pub(crate) mod run;
+//
+// pub mod prelude {
+//     pub use crate::app::app::App;
+//     pub use crate::app::routes::middleware_ctx::*;
+//     pub use crate::app::routes::action_ctx::*;
+//     pub use crate::app::routes::*;
+//     pub use crate::app::routes::req_local::*;
+//     pub use crate::app::routes::res::*;
+//     pub use crate::app::routes::req::*;
+//     pub use crate::app::routes::readonly_header_map::*;
+//     pub use crate::server::ReqCtx;
+//     pub use crate::app::routes::req::Req;
+//     pub use crate::app::routes::res::Res;
+//     pub use crate::core::graph::Graph;
+//     pub use teo_teon::value::Value;
+//     pub use teo_teon::teon;
+//     pub use teo_teon::teon_vec;
+//     pub use teo_teon::teon_unexpected;
+//     pub use teo_teon::teon_expect_expr_comma;
+//     pub use crate::core::object::Object;
+//     pub extern crate tokio;
+//     pub use tokio::main;
+//     pub extern crate key_path;
+//     pub use key_path::path;
+//     pub use crate::core::result::Result;
+//     pub use crate::core::error::Error;
+//     pub use crate::core::ctx::model::ModelCtx;
+//     pub use crate::core::ctx::user::UserCtx;
+// }
