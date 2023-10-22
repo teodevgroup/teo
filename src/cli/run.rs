f24ead8862f3b3818bdf195985967d472d9b7b4a
Victor Teo <victor.teo@fillmula.com>
2023-10-22 20:14:43 +0800

    connect database

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -1,15 +1,30 @@
 use teo_result::Result;
+use crate::app::ctx::Ctx;
+use crate::app::database::connect_databases;
 use crate::cli::command::{CLI, CLICommand};
 
 pub async fn run(cli: &CLI) -> Result<()> {
     match &cli.command {
-        CLICommand::Serve(serve_command) => {}
-        CLICommand::Generate(generate_command) => {}
-        CLICommand::Migrate(migrate_command) => {}
-        CLICommand::Seed(seed_command) => {}
-        CLICommand::Purge(purge_command) => {}
+        CLICommand::Serve(serve_command) => {
+            connect_databases(Ctx::main_namespace_mut())?;
+        }
+        CLICommand::Generate(generate_command) => {
+            todo!()
+        }
+        CLICommand::Migrate(migrate_command) => {
+            connect_databases(Ctx::main_namespace_mut())?;
+        }
+        CLICommand::Seed(seed_command) => {
+            connect_databases(Ctx::main_namespace_mut())?;
+        }
+        CLICommand::Purge(purge_command) => {
+            connect_databases(Ctx::main_namespace_mut())?;
+        }
         CLICommand::Lint(lint_command) => (),
-        CLICommand::Run(run_command) => (),
+        CLICommand::Run(run_command) => {
+            connect_databases(Ctx::main_namespace_mut())?;
+
+        },
     }
     Ok(())
 }
