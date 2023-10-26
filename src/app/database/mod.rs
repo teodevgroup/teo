820ef62f9071513e85de2c6548c2ee2f09805abd
Victor Teo <victor.teo@fillmula.com>
2023-10-26 22:23:27 +0800

    sql connection

--- src/app/database/mod.rs
+++ src/app/database/mod.rs
@@ -2,24 +2,36 @@ use std::sync::Arc;
 use teo_result::{Result};
 use teo_runtime::config::connector::Connector;
 use teo_runtime::connection::connection::Connection;
+use teo_runtime::database::database::Database;
 use teo_runtime::namespace::Namespace;
+use teo_sql_connector::connector::SQLConnection;
+use teo_sql_connector::schema::dialect::SQLDialect;
 
-pub fn connect_databases(namespace: &mut Namespace) -> Result<()> {
-    may_connect_database(namespace)?;
+pub async fn connect_databases(namespace: &mut Namespace) -> Result<()> {
+    may_connect_database(namespace).await?;
     for namespace in namespace.namespaces.values_mut() {
-        may_connect_database(namespace)?;
+        may_connect_database(namespace).await?;
     }
     Ok(())
 }
 
-pub fn may_connect_database(namespace: &mut Namespace) -> Result<()> {
+pub async fn may_connect_database(namespace: &mut Namespace) -> Result<()> {
     if namespace.connector.is_none() { return Ok(()) }
     let connector = namespace.connector.as_ref().unwrap();
-    let connection = connection_for_connector(connector);
+    let connection = connection_for_connector(connector).await;
     namespace.connection = Some(connection);
     Ok(())
 }
 
-fn connection_for_connector(connector: &Connector) -> Arc<dyn Connection> {
-    unreachable!()
+async fn connection_for_connector(connector: &Connector) -> Arc<dyn Connection> {
+    Arc::new(SQLConnection::new(
+        match connector.provider {
+            Database::MongoDB => unreachable!(),
+            Database::MySQL => SQLDialect::MySQL,
+            Database::PostgreSQL => SQLDialect::PostgreSQL,
+            Database::SQLite => SQLDialect::SQLite,
+        },
+        connector.url.as_str(),
+        false,
+    ).await)
 }
--- src/cli/run.rs
+++ src/cli/run.rs
@@ -7,31 +7,30 @@ use teo_runtime::connection::Ctx as ConnCtx;
 pub async fn run(cli: &CLI) -> Result<()> {
     match &cli.command {
         CLICommand::Serve(serve_command) => {
-            connect_databases(Ctx::main_namespace_mut())?;
+            connect_databases(Ctx::main_namespace_mut()).await?;
             let ctx = ConnCtx::from_namespace(Ctx::main_namespace());
         }
         CLICommand::Generate(generate_command) => {
             todo!()
         }
         CLICommand::Migrate(migrate_command) => {
-            connect_databases(Ctx::main_namespace_mut())?;
+            connect_databases(Ctx::main_namespace_mut()).await?;
         }
         CLICommand::Seed(seed_command) => {
-            connect_databases(Ctx::main_namespace_mut())?;
+            connect_databases(Ctx::main_namespace_mut()).await?;
         }
         CLICommand::Purge(purge_command) => {
-            connect_databases(Ctx::main_namespace_mut())?;
+            connect_databases(Ctx::main_namespace_mut()).await?;
         }
         CLICommand::Lint(lint_command) => (),
         CLICommand::Run(run_command) => {
-            connect_databases(Ctx::main_namespace_mut())?;
+            connect_databases(Ctx::main_namespace_mut()).await?;
             if let Some(program) = Ctx::get_mut().programs.get(&run_command.name) {
                 let ctx = ConnCtx::from_namespace(Ctx::main_namespace());
                 program.call(ctx).await?
             } else {
                 Err(Error::new(format!("program '{}' is not defined", &run_command.name)))?
             }
-
         },
     }
     Ok(())
