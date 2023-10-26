06003950716e64d42f087fbf90f281f528b8283f
Victor Teo <victor.teo@fillmula.com>
2023-10-26 20:43:12 +0800

    fix

--- src/core/enum.rs
+++
@@ -1,4 +0,0 @@
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
-pub struct DbEnum {
-    pub(crate) choices: Vec<String>,
-}
--- src/core/model/index.rs
+++ src/core/model/index.rs
@@ -77,16 +77,6 @@ impl ModelIndex {
         }
     }
 
-    pub(crate) fn psql_primary_to_unique(&self, table_name: &str) -> Self {
-        let mut result = Self {
-            index_type: ModelIndexType::Unique,
-            name: None,
-            items: self.items.clone(),
-            keys: self.keys.clone(),
-        };
-        result.name = Some(format!("{table_name}_{}_pkey", self.joined_names()));
-        result
-    }
 
     pub(crate) fn r#type(&self) -> ModelIndexType {
         self.index_type
@@ -99,13 +89,7 @@ impl ModelIndex {
         }
     }
 
-    pub(crate) fn sql_name(&self, table_name: &str, dialect: SQLDialect) -> Cow<str> {
-        if self.name.is_some() {
-            Cow::Borrowed(self.name.as_ref().unwrap().as_str())
-        } else {
-            Cow::Owned(self.normalize_name(table_name, dialect))
-        }
-    }
+
 
     pub(crate) fn mongodb_name(&self) -> String {
         if self.name.is_some() {
@@ -131,86 +115,6 @@ impl ModelIndex {
         self.keys.iter().map(|s| *s).collect::<HashSet<&'static str>>()
     }
 
-    pub(crate) fn to_sql_drop(&self, dialect: SQLDialect, table_name: &str) -> String {
-        let escape = dialect.escape();
-        let index_name_cow = self.sql_name(table_name, dialect);
-        let index_name = index_name_cow.as_ref();
-        if dialect == SQLDialect::PostgreSQL {
-            format!("DROP INDEX {escape}{index_name}{escape}")
-        } else {
-            format!("DROP INDEX {escape}{index_name}{escape} ON {escape}{table_name}{escape}")
-        }
-    }
-
-    pub(crate) fn to_sql_create(&self, dialect: SQLDialect, table_name: &str) -> String {
-        let escape = dialect.escape();
-        let index_name_cow = self.sql_name(table_name, dialect);
-        let index_name = index_name_cow.as_ref();
-        let unique = if self.r#type().is_unique() { "UNIQUE " } else { "" };
-        let fields: Vec<String> = self.items.iter().map(|item| {
-            Self::sql_format_item(dialect, item, false)
-        }).collect();
-        format!("CREATE {unique}INDEX {escape}{index_name}{escape} ON {escape}{table_name}{escape}({})", fields.join(","))
-    }
-
-    pub(crate) fn sql_format_item(dialect: SQLDialect, item: &ModelIndexItem, table_create_mode: bool) -> String {
-        let escape = dialect.escape();
-        let name = item.field_name();
-        let sort = item.sort().to_str();
-        let len = if let Some(len) = item.len() {
-            if dialect == SQLDialect::MySQL {
-                Cow::Owned(format!("({})", len))
-            } else {
-                Cow::Borrowed("")
-            }
-        } else {
-            Cow::Borrowed("")
-        };
-        if table_create_mode && dialect == SQLDialect::PostgreSQL {
-            format!("{escape}{name}{escape}")
-        } else {
-            format!("{escape}{name}{escape}{len} {sort}")
-        }
-    }
-
-    pub(crate) fn normalize_name(&self, table_name: &str, dialect: SQLDialect) -> String {
-        match self.index_type {
-            ModelIndexType::Primary => match dialect {
-                SQLDialect::MySQL => "PRIMARY".to_owned(),
-                SQLDialect::SQLite => format!("sqlite_autoindex_{}_1", table_name),
-                SQLDialect::PostgreSQL => self.normalize_name_psql(table_name),
-                _ => unreachable!()
-            },
-            _ => match dialect {
-                SQLDialect::PostgreSQL => self.normalize_name_psql(table_name),
-                _ => self.normalize_name_normal(table_name),
-            }
-        }
-    }
-
-    fn normalize_name_normal(&self, table_name: &str) -> String {
-        format!("{table_name}_{}", self.joined_names())
-    }
-
-    fn normalize_name_psql(&self, table_name: &str) -> String {
-        if self.index_type.is_primary() {
-            format!("{table_name}_{}", self.psql_suffix())
-        } else {
-            format!("{table_name}_{}_{}", self.joined_names(), self.psql_suffix())
-        }
-    }
-
-    fn psql_suffix(&self) -> &str {
-        if self.index_type.is_primary() {
-            "pkey"
-        } else {
-            "idx"
-        }
-    }
-
-    fn joined_names(&self) -> String {
-        self.keys.join("_")
-    }
 
     pub(crate) fn append_item(&mut self, item: ModelIndexItem) {
         let key = Box::leak(Box::new(item.field_name().to_string()));
