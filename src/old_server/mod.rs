84999919c953b363e96743e5e2e60db401e4cedc
Victor Teo <victor.teo@fillmula.com>
2023-10-28 00:00:06 +0800

    remove find many

--- src/old_server/mod.rs
+++ src/old_server/mod.rs
@@ -175,42 +175,6 @@ async fn handle_find_first(graph: &'static Graph, input: &Value, model: &'static
     }
 }
 
-async fn handle_find_many(graph: &'static Graph, input: &Value, model: &'static Model, source: Initiator, connection: Arc<dyn Connection>) -> Result<Res> {
-    let action = Action::from_u32(FIND | MANY | ENTRY);
-    let result = graph.find_many_internal(model, input, false, action, source, connection.clone()).await;
-    match result {
-        Ok(results) => {
-            let mut count_input = input.clone();
-            let count_input_obj = count_input.as_hashmap_mut().unwrap();
-            count_input_obj.remove("skip");
-            count_input_obj.remove("take");
-            count_input_obj.remove("pageSize");
-            count_input_obj.remove("pageNumber");
-            let count = graph.count(model, &count_input, connection.clone()).await.unwrap();
-            let mut meta = teon!({"count": count});
-            let page_size = input.get("pageSize");
-            if page_size.is_some() {
-                let page_size = page_size.unwrap().as_i32().unwrap();
-                let count = count as i32;
-                let mut number_of_pages = count / page_size;
-                if count % page_size != 0 {
-                    number_of_pages += 1;
-                }
-                meta.as_hashmap_mut().unwrap().insert("numberOfPages".to_string(), number_of_pages.into());
-            }
-
-            let mut result_json: Vec<Value> = vec![];
-            for (index, result) in results.iter().enumerate() {
-                match result.to_json_internal(&path!["data", index]).await {
-                    Ok(result) => result_json.push(result),
-                    Err(_) => return Err(Error::permission_error(path!["data"], "not allowed to read")),
-                }
-            }
-            Ok(Res::TeonDataMetaRes(Value::Vec(result_json), meta))
-        },
-        Err(err) => Err(err)
-    }
-}
 
 async fn handle_create_internal<'a>(graph: &'static Graph, create: Option<&'a Value>, include: Option<&'a Value>, select: Option<&'a Value>, model: &'static Model, path: &'a KeyPath<'_>, action: Action, action_source: Initiator, connection: Arc<dyn Connection>) -> Result<Value> {
     let obj = graph.new_object(model, action, action_source, connection)?;
