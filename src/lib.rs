cc3ce67b0f7713c340087527dfabefd72fb6e0f2
Victor Teo <victor.teo@fillmula.com>
2023-11-02 06:09:42 +0800

    fix for rust enum generation

--- src/lib.rs
+++ src/lib.rs
@@ -17,30 +17,11 @@ pub mod prelude {
     pub use teo_runtime::response::Response;
     pub use teo_runtime::path;
     pub use teo_runtime::traits::generated_enum_to_value::{GeneratedEnumToValue, ValueToGeneratedEnum};
+    pub use teo_runtime::model;
+    pub use teo_runtime::interface;
+    pub use teo_teon::value::Value;
+    pub use teo_teon::teon;
+    pub use teo_teon::teon_vec;
+    pub use teo_teon::teon_unexpected;
+    pub use teo_teon::teon_expect_expr_comma;
 }
-
-// pub(crate) mod old_seeder;
-// pub(crate) mod purge;
-
-// pub mod prelude {
-//     pub use crate::old_app::old_app::App;
-//     pub use crate::old_app::routes::middleware_ctx::*;
-//     pub use crate::old_app::routes::action_ctx::*;
-//     pub use crate::old_app::routes::*;
-//     pub use crate::old_app::routes::req_local::*;
-//     pub use crate::old_app::routes::res::*;
-//     pub use crate::old_app::routes::req::*;
-//     pub use crate::old_app::routes::readonly_header_map::*;
-//     pub use crate::old_server::ReqCtx;
-//     pub use crate::old_app::routes::req::Req;
-//     pub use crate::old_app::routes::res::Res;
-//     pub use crate::old_core::graph::Graph;
-//     pub use teo_teon::value::Value;
-//     pub use teo_teon::teon;
-//     pub use teo_teon::teon_vec;
-//     pub use teo_teon::teon_unexpected;
-//     pub use teo_teon::teon_expect_expr_comma;
-//     pub use crate::old_core::object::Object;
-//     pub use crate::old_core::ctx::model::ModelCtx;
-//     pub use crate::old_core::ctx::user::UserCtx;
-// }
