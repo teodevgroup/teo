0474ed360d84d3ef00ab5a864ef050adbb6dcb54
Victor Teo <victor.teo@fillmula.com>
2024-02-22 11:00:46 +0800

    update

--- src/cli/parse.rs
+++ src/cli/parse.rs
@@ -129,6 +129,7 @@ pub(crate) fn parse(runtime_version: RuntimeVersion, entrance: Entrance) -> CLI 
                 .num_args(1)))
         .get_matches_from(match runtime_version {
             RuntimeVersion::Python(_) | RuntimeVersion::NodeJS(_) => {
+                println!("see result before handle: {:?}", env::args_os());
                 let result = env::args_os().enumerate().filter(|(i, x)| (*i != 1) && (!x.to_str().unwrap().ends_with("ts-node") && !x.to_str().unwrap().ends_with(".ts"))).map(|(_i, x)| x).collect::<Vec<OsString>>();
                 println!("see result {:?}", result);
                 result
