5107e61c305edb725193c7a22a2cdbcbbe8d8a1f
Victor Teo <victor.teo@fillmula.com>
2024-02-26 15:57:46 +0800

    remove old test context

--- src/old_code/old_server/test_context.rs
+++
@@ -1,7 +0,0 @@
-use crate::core::conf::test::ResetMode;
-use crate::seeder::data_set::DataSet;
-
-pub(crate) struct TestContext {
-    pub(crate) reset_mode: ResetMode,
-    pub(crate) datasets: Vec<DataSet>,
-}
