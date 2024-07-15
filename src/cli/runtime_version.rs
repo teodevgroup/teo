32d6e4e9bdf33300fab1eef84db6b71d2add3b79
Victor Teo <victor.teo@fillmula.com>
2024-07-15 14:30:14 +0800

    fix

--- src/cli/runtime_version.rs
+++ src/cli/runtime_version.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Clone, Debug)]
 pub enum RuntimeVersion {
     Rust(&'static str),
@@ -5,13 +7,13 @@ pub enum RuntimeVersion {
     Python(String),
 }
 
-impl ToString for RuntimeVersion {
-
-    fn to_string(&self) -> String {
-        match self {
+impl Display for RuntimeVersion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
             RuntimeVersion::Rust(v) => format!("Rust {v}"),
             RuntimeVersion::NodeJS(v) => format!("Node.js {v}"),
             RuntimeVersion::Python(v) => format!("Python {v}"),
-        }
+        };
+        write!(f, "{}", str)
     }
 }
