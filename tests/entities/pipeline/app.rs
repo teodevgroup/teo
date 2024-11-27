cbdb87e00e1ee7c26e60bd28410897452764a71e
Victor Teo <victor.teo@fillmula.com>
2024-11-27 21:27:39 +0800

    fix

--- tests/entities/pipeline/app.rs
+++ tests/entities/pipeline/app.rs
@@ -3,6 +3,7 @@ use teo_runtime::request::Request;
 use teo_runtime::response::Response;
 use teo_runtime::{pipeline, request, teon, Value};
 use teo_runtime::arguments::Arguments;
+use teo_runtime::pipeline::item::templates::validator::{ValidatorResult, Validity};
 use teo::app::App;
 use teo::result::Result;
 use teo::test::schema_path::schema_path_args;
@@ -13,8 +14,8 @@ pub fn load_app() -> Result<App> {
         schema_path_args(file!(), "schema.teo")
     )?;
     app.main_namespace().define_validator_pipeline_item("myValidator", |args: Arguments| {
-        return Ok(|ctx: pipeline::Ctx| async {
-            !ctx.value().is_null()
+        return Ok(|_: Value, ctx: pipeline::Ctx| async move {
+            ValidatorResult::Validity(Validity::Valid)
         })
     });
     Ok(app)
