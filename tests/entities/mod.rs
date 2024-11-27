4ac8d813397c907bd7267f790ddfeeca2f696c13
Victor Teo <victor.teo@fillmula.com>
2024-11-27 21:04:54 +0800

    add new test case

--- tests/entities/mod.rs
+++ tests/entities/mod.rs
@@ -1,2 +1,3 @@
 pub mod models;
 pub mod requests;
+pub mod pipeline;
---
+++ tests/entities/pipeline/app.rs
@@ -0,0 +1,21 @@
+use futures_util::future::try_join_all;
+use teo_runtime::request::Request;
+use teo_runtime::response::Response;
+use teo_runtime::{pipeline, request, teon, Value};
+use teo_runtime::arguments::Arguments;
+use teo::app::App;
+use teo::result::Result;
+use teo::test::schema_path::schema_path_args;
+use crate::entities::models::types::entities::{SupportCreateInput, SupportFindManyArgs, Teo};
+
+pub fn load_app() -> Result<App> {
+    let app = App::new_with_argv(
+        schema_path_args(file!(), "schema.teo")
+    )?;
+    app.main_namespace().define_validator_pipeline_item("myValidator", |args: Arguments| {
+        return Ok(|ctx: pipeline::Ctx| async {
+            !ctx.value().is_null()
+        })
+    });
+    Ok(app)
+}
