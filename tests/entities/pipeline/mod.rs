70dc80a8bf58031eeb7284809501200f22489a66
Victor Teo <victor.teo@fillmula.com>
2024-11-29 22:56:35 +0800

    array transformers

--- tests/entities/pipeline/mod.rs
+++ tests/entities/pipeline/mod.rs
@@ -227,4 +227,193 @@ mod tests {
             })
         }))
     }
+
+    #[serial]
+    #[shared_tokio_runtime::runtime_test]
+    async fn transform_int32_array() {
+        before_all().await;
+        before_each().await;
+        let req = TestRequest::new(Method::POST, "/Container/create")
+            .json_body(json!({
+                "create": {
+                    "int32Array": [1, 1],
+                },
+            }))
+            .await.unwrap();
+        let res = server().process_test_request(req).await.unwrap().body_as_json().unwrap();
+        assert_json!(res, matcher!({
+            "data": partial({
+                "id": ignore,
+                "int32Array": [10, 10],
+            })
+        }))
+    }
+
+    #[serial]
+    #[shared_tokio_runtime::runtime_test]
+    async fn transform_int64_array() {
+        before_all().await;
+        before_each().await;
+        let req = TestRequest::new(Method::POST, "/Container/create")
+            .json_body(json!({
+                "create": {
+                    "int64Array": [1, 1],
+                },
+            }))
+            .await.unwrap();
+        let res = server().process_test_request(req).await.unwrap().body_as_json().unwrap();
+        assert_json!(res, matcher!({
+            "data": partial({
+                "id": ignore,
+                "int64Array": [10, 10],
+            })
+        }))
+    }
+
+    #[serial]
+    #[shared_tokio_runtime::runtime_test]
+    async fn transform_float32_array() {
+        before_all().await;
+        before_each().await;
+        let req = TestRequest::new(Method::POST, "/Container/create")
+            .json_body(json!({
+                "create": {
+                    "float32Array": [1.0, 1.0],
+                },
+            }))
+            .await.unwrap();
+        let res = server().process_test_request(req).await.unwrap().body_as_json().unwrap();
+        assert_json!(res, matcher!({
+            "data": partial({
+                "id": ignore,
+                "float32Array": [10.0, 10.0],
+            })
+        }))
+    }
+
+    #[serial]
+    #[shared_tokio_runtime::runtime_test]
+    async fn transform_float64_array() {
+        before_all().await;
+        before_each().await;
+        let req = TestRequest::new(Method::POST, "/Container/create")
+            .json_body(json!({
+                "create": {
+                    "float64Array": [1.0, 1.0],
+                },
+            }))
+            .await.unwrap();
+        let res = server().process_test_request(req).await.unwrap().body_as_json().unwrap();
+        assert_json!(res, matcher!({
+            "data": partial({
+                "id": ignore,
+                "float64Array": [10.0, 10.0],
+            })
+        }))
+    }
+
+    #[serial]
+    #[shared_tokio_runtime::runtime_test]
+    async fn transform_bool_array() {
+        before_all().await;
+        before_each().await;
+        let req = TestRequest::new(Method::POST, "/Container/create")
+            .json_body(json!({
+                "create": {
+                    "boolArray": [false, false],
+                },
+            }))
+            .await.unwrap();
+        let res = server().process_test_request(req).await.unwrap().body_as_json().unwrap();
+        assert_json!(res, matcher!({
+            "data": partial({
+                "id": ignore,
+                "boolArray": [true, true],
+            })
+        }))
+    }
+
+    #[serial]
+    #[shared_tokio_runtime::runtime_test]
+    async fn transform_date_array() {
+        before_all().await;
+        before_each().await;
+        let req = TestRequest::new(Method::POST, "/Container/create")
+            .json_body(json!({
+                "create": {
+                    "dateArray": ["2005-06-01", "2005-06-01"],
+                },
+            }))
+            .await.unwrap();
+        let res = server().process_test_request(req).await.unwrap().body_as_json().unwrap();
+        assert_json!(res, matcher!({
+            "data": partial({
+                "id": ignore,
+                "dateArray": [date_value("2005-06-02"), date_value("2005-06-02")],
+            })
+        }))
+    }
+
+    #[serial]
+    #[shared_tokio_runtime::runtime_test]
+    async fn transform_date_time_array() {
+        before_all().await;
+        before_each().await;
+        let req = TestRequest::new(Method::POST, "/Container/create")
+            .json_body(json!({
+                "create": {
+                    "dateTimeArray": ["2024-11-29T14:49:13.498Z", "2024-11-29T14:49:13.498Z"],
+                },
+            }))
+            .await.unwrap();
+        let res = server().process_test_request(req).await.unwrap().body_as_json().unwrap();
+        assert_json!(res, matcher!({
+            "data": partial({
+                "id": ignore,
+                "dateTimeArray": [date_time_value("2024-11-30T14:49:13.498Z"), date_time_value("2024-11-30T14:49:13.498Z")],
+            })
+        }))
+    }
+
+    #[serial]
+    #[shared_tokio_runtime::runtime_test]
+    async fn transform_decimal_array() {
+        before_all().await;
+        before_each().await;
+        let req = TestRequest::new(Method::POST, "/Container/create")
+            .json_body(json!({
+                "create": {
+                    "decimalArray": ["1", "1"],
+                },
+            }))
+            .await.unwrap();
+        let res = server().process_test_request(req).await.unwrap().body_as_json().unwrap();
+        assert_json!(res, matcher!({
+            "data": partial({
+                "id": ignore,
+                "decimalArray": [decimal_value("10"), decimal_value("10")],
+            })
+        }))
+    }
+
+    #[serial]
+    #[shared_tokio_runtime::runtime_test]
+    async fn transform_status_array() {
+        before_all().await;
+        before_each().await;
+        let req = TestRequest::new(Method::POST, "/Container/create")
+            .json_body(json!({
+                "create": {
+                    "statusArray": ["open", "open"],
+                },
+            }))
+            .await.unwrap();
+        let res = server().process_test_request(req).await.unwrap().body_as_json().unwrap();
+        assert_json!(res, matcher!({
+            "data": partial({
+                "id": ignore,
+                "statusArray": ["pending", "pending"],
+            })
+        }))
+    }
 }
