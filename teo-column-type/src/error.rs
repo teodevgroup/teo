3cc486996ca0f1b5b9b22f5662f9198d17032b42
Victor Teo <victor.teo@fillmula.com>
2026-02-09 22:07:37 +0800

    fix

--- teo-column-type/src/error.rs
+++ teo-column-type/src/error.rs
@@ -11,8 +11,8 @@ impl Error {
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result {
-        f.write_str("Unknown column type: ");
-        f.write_str(&self.0);
+        f.write_str("Unknown column type: ")?;
+        f.write_str(&self.0)?;
         Ok(())
     }
 }
