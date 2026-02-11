403d7fd611f5da9d311db7494198205f8aaafe32
Victor Teo <victor.teo@fillmula.com>
2026-02-11 10:18:07 +0800

    remove unused sql ty

--- teo-derive/src/utils/mod.rs
+++ teo-derive/src/utils/mod.rs
@@ -1,7 +1,5 @@
 mod is_std_option;
-mod sql_ty;
 mod extract_first_path_argument;
 
 pub(crate) use is_std_option::is_std_option;
-pub(crate) use sql_ty::sql_ty;
 pub(crate) use extract_first_path_argument::extract_first_path_argument;
