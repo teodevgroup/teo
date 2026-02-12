3255681a33a04aa58002a2a2790136ceb1c33168
Victor Teo <victor.teo@fillmula.com>
2026-02-12 13:01:14 +0800

    update test

---
+++ teo/tests/decl.rs
@@ -0,0 +1,16 @@
+use teo::Entity;
+use teo::teo_column_type::postgres::ColumnType;
+
+#[derive(Entity)]
+#[teo(table_name = "User")]
+#[teo(index(
+    column(name = a, order = "asc"),
+    column(name = b),
+    name = "my_index"))]
+struct User {
+    #[teo(column_name = "a", primary)]
+    #[teo(postgres(column_type = "text"))]
+    a: String,
+    #[teo(postgres(column_type = "int"))]
+    b: i32,
+}
--- teo/tests/mod.rs
+++ teo/tests/mod.rs
@@ -1,18 +1,2 @@
 mod connectors;
-
-use teo_derive::Entity;
-use teo::teo_column_type::postgres::ColumnType;
-
-#[derive(Entity)]
-#[teo(table_name = "User")]
-#[teo(index(
-    column(name = a, order = "asc"),
-    column(name = b),
-    name = "my_index"))]
-struct User {
-    #[teo(column_name = "a", primary)]
-    #[teo(postgres(column_type = "text"))]
-    a: String,
-    #[teo(postgres(column_type = "int"))]
-    b: i32,
-}
+mod decl;
